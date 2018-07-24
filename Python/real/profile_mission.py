cb98d32987e5d520af8600765741de7782fec9f5
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-07-24 15:36:16 -0300

    countdown + flight profile add

--- Python/real/profile_mission.py
+++ Python/real/profile_mission.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 #
 ###############################################
-# AUTOR: lenonr
+# AUTHOR: lenonr
 #
-# VERSAO: 0.10
+# VERSION: 0.10
 #
-# DATA DE CRIACAO: 23/07/18
-# ULTIMA MODIFICACAO: 23/07/18
+# CREATION DATE: 23/07/18
+# LAST MODIFICATION: 23/07/18
 #
-# DESCRICAO: 
-#	Script para mostrar o tempo dos status principais do foguete,
-#	como MECO, Separation, SECO, Deploy, etc.
-#
-#	Basta digitar os valores de cada status, para que o script 
-#	mostre alguns segundos antes para o usuario.
+# DESCRIPTION: 
+#	Show time profile by type rocket, one by one.
 #
 ###############################################
 # BODY
@@ -24,24 +20,32 @@ os.system('cls' if os.name == 'nt' else 'clear')
 ###############################################
 ## SETAR TEMPO AUTOMATICAMENTE
 # style launch
-profile = [ariane, soyuz, falcon9_nolanding, falcon9_landing, falconh]
+profile = ["ariane", "soyuz", "falcon9_nolanding", "falcon9_landing", "falconh"]
 
 # status profile
-ariane = [liftoff, maxq, sepation1, meco, sepation2, seco, separation3, deploy]
-soyuz  = [liftoff, maxq, sepation1, meco, sepation2, seco, sepation2, deploy]
-falcon9_landing = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
-falcon9_nolanding  = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
-falconh  = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
+sequence_ariane = [ "ignition_mainstage", "ignition_solidbooster", "liftoff", "pitch", "roll", 
+					"eap_separation", "fairing", "epc_separation", "eps_ignition1", "shutdown_eps1",  
+		  			"eps_ignition2", "shutdown_eps2" ]
+		  
+countdown_ariane = [ "0s", "7s", "12s", "17s", 
+		  			 "2m19s", "3m44s", "9m01s", "9m19s", "19m58s",
+		  			 "3h27m50s", "3h34m08s" ]
+
+# soyuz  = [liftoff, maxq, sepation1, meco, sepation2, seco, sepation2, deploy]
+# falcon9_landing = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
+# falcon9_nolanding  = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
+# falconh  = [liftoff, maxq, meco, sepation1, seco, reentry_burn, landing, deploy]
 
 ## FUNCION PROFILE MISSION
 ###############################################
 def rocket_profile_mission():
-	name_rocket = raw_input("The Vehicle for Launch: ")
+	print ("Profile's availables: " + str(profile))
 
-## MAIN FUNCION
-###############################################
-def main():	
-	pass
+	rocket_name = raw_input("The Vehicle for Launch: ")	
+
+	# verify rocket
+	if rocket_name == "ariane":
+		print (countdown_ariane)
 
 ## CALL SCRIPT
-main()
+rocket_profile_mission()
