ca89d7f79c0169835176ed4267a2222f0707c3c3
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-08-19 13:43:41 -0300

    funcionando perfeitamente

--- Python/kerbal/falkinho/modules/orbita.py
+++ Python/kerbal/falkinho/modules/orbita.py
@@ -3,12 +3,13 @@
 ## import modules
 from base import launch
 
-turn_start_altitude 	= 1000
-turn_end_altitude 		= 45000
-target_altitude 		= 140000
-maxq_begin 				= 20000
-maxq_end   				= turn_end_altitude
-correction_time 		= 0.01
+# variables default
+turn_start_altitude     = 1000
+turn_end_altitude       = 45000
+target_altitude         = 140000
+maxq_begin              = 25000
+maxq_end                = 70000
+correction_time         = 0.1
 
 # start script
 launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time)
