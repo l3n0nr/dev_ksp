8cc9b393d034ce5a54ec5f9d4f1c28cd96e86a61
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-08-20 16:42:31 -0300

    arquivo melhorado

--- Python/kerbal/falkinho/pouso.py
+++ Python/kerbal/falkinho/pouso.py
@@ -7,6 +7,11 @@ import math
 import time
 import krpc
 
+from numpy.linalg import *
+from numpy import array
+from time import sleep
+from math import exp,sqrt,cos,pi,acos
+
 conn = krpc.connect(name='Launch into orbit')
 vessel = conn.space_center.active_vessel
 ksc = conn.space_center
@@ -41,9 +46,9 @@ massaseca = conn.add_stream(getattr, nave, 'dry_mass')
 def launch():   
     turn_start_altitude     = 1000
     turn_end_altitude       = 45000
-    target_altitude         = 84500
+    target_altitude         = 78000
     maxq_begin              = 12000
-    maxq_end                = target_altitude
+    maxq_end                = 70000
     correction_time         = 0.05
 
     print('Systems nominal for launch. T-3 seconds!')
@@ -124,128 +129,210 @@ def launch():
     while altitude() < 70500:
         pass
 
-def calcula_gravidade(AltitudeNave, nave):
-    parametro_gravitacional = nave.orbit.body.gravitational_parameter
-    RaioAstro = nave.orbit.body.equatorial_radius
-    R = RaioAstro + AltitudeNave()
-    g = parametro_gravitacional / (R ** 2.)
-    return -g
-
-def calcula_queima_suicida(Velocidade, Impulso, Massa, altitude, nave):
-    global altitude_nivel_mar
-    margem_de_seguranca = 1.0 #se não está dando tempo de reduzir velocidade, tente entre 1.1 e 1.2
-    gravidade = calcula_gravidade(altitude, nave)
-    va = ((Impulso()/Massa()) + gravidade)
-    A = (Velocidade() ** 2) / (2 * va)
-    return A * margem_de_seguranca		
-
-def ControlePouso(nave, velocidade_vertical, velocidade, altitude, massatotal, impulso, atmosfera = True):
-    print(atmosfera, altitude(), (calcula_queima_suicida(velocidade, impulso, massatotal, altitude, nave) - velocidade_vertical()), velocidade(), -velocidade_vertical())
-    if altitude() < 3000:
-        if altitude() <= (calcula_queima_suicida(velocidade, impulso, massatotal, altitude, nave) - velocidade_vertical()):
-            nave.control.throttle = 1.0
-
-    if velocidade_vertical() > -15:
-        gravidade = calcula_gravidade(altitude, nave)
-        try:
-            zeraraceleracao = (massatotal() * -gravidade) / impulso()
-        except:
-            zeraraceleracao = nave.control.throttle
-        if atmosfera:
-            if velocidade_vertical() > -15:
-                if velocidade_vertical() < -5 and velocidade_vertical() > -15:
-                    nave.control.throttle = zeraraceleracao
-                elif velocidade_vertical() > -5:
-                    nave.control.throttle = zeraraceleracao - 0.1
-                elif velocidade_vertical() < -10:
-                    nave.control.throttle = zeraraceleracao + 0.1
-        else:
-            if altitude() > 90 and altitude() < 300:
-                if velocidade_vertical() < -9 and velocidade_vertical() > -10:
-                    nave.control.throttle = zeraraceleracao
-                elif velocidade_vertical() > -9:
-                    nave.control.throttle = zeraraceleracao - 0.1
-                elif velocidade_vertical() < -10:
-                    nave.control.throttle = zeraraceleracao + 0.1
-            elif altitude() > 30 and altitude() < 90:
-                if velocidade_vertical() < -5 and velocidade_vertical() > -6:
-                    nave.control.throttle = zeraraceleracao
-                elif velocidade_vertical() > -5:
-                    nave.control.throttle = zeraraceleracao - 0.1
-                elif velocidade_vertical() < -6:
-                    nave.control.throttle = zeraraceleracao + 0.1
-            elif altitude() > 20 and altitude() < 30:
-                if velocidade_vertical() < -1 and velocidade_vertical() > -3:
-                    nave.control.throttle = zeraraceleracao
-                elif velocidade_vertical() > -1:
-                    nave.control.throttle = zeraraceleracao - 0.1
-                elif velocidade_vertical() < -3:
-                    nave.control.throttle = zeraraceleracao + 0.1
-            elif altitude() < 20:
-                if velocidade_vertical() < -0.75 and velocidade_vertical() > -1.25:
-                    nave.control.throttle = zeraraceleracao
-                elif velocidade_vertical() > -0.5:
-                    nave.control.throttle = zeraraceleracao - 0.1
-                elif velocidade_vertical() < -1.1:
-                    nave.control.throttle = zeraraceleracao + 0.1
+def curse_centered_addstr(l,pos,scr):
+    my,mx=scr.getmaxyx()
     try:
-        if velocidade_horizontal() > 300.0:
-            if nave.control.sas_mode is not ksc.SASMode.retrograde:
-                nave.control.sas_mode = ksc.SASMode.retrograde
-        else:
-            if nave.control.sas_mode is not ksc.SASMode.radial:
-                nave.control.rcs = True
-                nave.control.sas_mode = ksc.SASMode.radial
-                nave.auto_pilot.sas_mode = ksc.SASMode.radial
-    except:
+        scr.addstr(pos,mx/2-len(l)/2,l)
+    except ERR:
+        scr.addstr(my-1,0,"ERROR IN PRINTING")
         pass
 
-def landing():    	
-	print ('Reentry core')
-	# nave.control.rcs = False
-	# nave.control.sas = True
-	# nave.control.brakes = True
 
-	altitude_mantis = conn.add_stream(getattr, nave.flight(rf), 'surface_altitude')
-	velocidade_mantis = conn.add_stream(getattr, nave.flight(rf), 'speed')
-	velocidade_vertical_mantis = conn.add_stream(getattr, nave.flight(rf), 'vertical_speed')
-	impulso_mantis = conn.add_stream(getattr, nave, 'max_thrust')
-	massatotal_mantis = conn.add_stream(getattr, nave, 'mass')
-	
-	def pousar(nave):
-		global t, Mantis
-		del(t)
-		t = tela()
-		nave.auto_pilot.sas = True
-		nave.auto_pilot.rcs = True
-		try:
-			nave.auto_pilot.sas_mode = ksc.SASMode.retrograde
-		except:
-			pass
-		sleep(5)
-		ksc.warp_to(ut() + 180)
-		while not nave.parts.legs[0].is_grounded:
-			if altitude_mantis() < 5000:
-				ControlePouso(nave, velocidade_vertical_mantis, velocidade_mantis, altitude_mantis, massatotal_mantis, impulso_mantis, True)
-	        if not nave.control.brakes:
-	            if altitude_mantis() < 50000:
-	                nave.control.brakes = True
-	                # t.mensagem('Ativando aerofreios')
-	                sleep(2)
-	                # t.mensagem('')
-	        if not nave.control.gear:
-	            if velocidade_vertical_mantis() > -100 and altitude_mantis() < 500:
-	                nave.control.gear = True
-	                # t.mensagem('Baixando as pernas')
-		nave.control.throttle = 0.0
-		nave.control.brakes = False
-		nave.auto_pilot.sas = True
-		nave.auto_pilot.sas_mode = ksc.SASMode.radial
-	    # t.mensagem('')
-	    # del(t)
+def stage(vess):
+    vess.control.activate_next_stage()
+
+class runmode:
+    def __init__(self,mode=0):
+        self.mode = mode
+    def __add__(self,n):
+        self.mode = self.mode+n
+    def __sub__(self,n):
+        self.mode = self.mode-n
+    def reset(self):
+        self.mode = 0
+    def finish(self):
+        self.mode = -1
+    def __nonzero__(self):
+        return not self.mode == -1
+    def __call__(self,n):
+        return self.mode == n
+    def __str__(self):
+        return "Run Mode: "+str(self.mode)
+
+def check_engines(vessel):
+    for eng in vessel.parts.engines:
+        if not eng.has_fuel:
+            stage(vessel)
+            break
+
+def tnorm(tu):
+    return float(linalg.norm(array(tu)))
+
+def executemaneuver(conn):
+    vessel = conn.space_center.active_vessel
+    control = vessel.control
+    ap = vessel.auto_pilot
+    ap.reference_frame = vessel.orbit.body.reference_frame
+    ap.set_pid_parameters(30,0,6)
+    ap.engage()
+
+    if len(control.nodes)<1:
+        raise NameError('NoNode')
+
+
+    rm = runmode()
+
+    mynode = control.nodes[0]
+
+
+    while rm:
+        if rm(0):
+            dv =  mynode.delta_v
+            ########################
+            #Only for 1-engine vessels
+            for eng in vessel.parts.engines:
+                if eng.active:
+                    Isp=eng.specific_impulse
+                    break
+            ########################
+            mdot = vessel.max_thrust/9.82/Isp #mass flowrate
+            mfoverm0 = exp(-dv/Isp/9.82)
+            dt = vessel.mass*(1-mfoverm0)/mdot
+            rm+1
+        if rm(1):
+            ap.target_direction=mynode.burn_vector(vessel.orbit.body.reference_frame)
+            print "ETA: {0:0.2f}".format(mynode.time_to-dt/2)
+            if mynode.time_to<dt/2:
+                rm+1
+        if rm(2):
+            dvvec=mynode.remaining_burn_vector(vessel.orbit.body.reference_frame)
+            ap.target_direction=dvvec
+            tcommand = 0.07*mynode.remaining_delta_v
+            control.throttle = min(max(tcommand,0),1)
+            if tnorm(dvvec)<0.2:
+                rm.finish()
+
+
+    control.throttle = 0
+
+def landing():      
+    print ('Reentry core')      
+
+    # if altitude() <= airbrakes:
+    #    nave.control.brakes = True
+
+    # if altitude() <= legs:
+    #     nave.control.gear = True
+
+    conn = krpc.connect(name='Land Program')
+    vessel = conn = krpc.connect(name='Land Program')
+    vessel = conn.space_center.active_vessel
+    control = vessel.control
+    ap = vessel.auto_pilot
+    ap.reference_frame = vessel.surface_velocity_reference_frame
+    # ap.set_pid_parameters(20,0,4)
+    ap.engage()
+
+    ksc = conn.space_center
+    nave = ksc.active_vessel
+    rf = nave.orbit.body.reference_frame
+    altitude = conn.add_stream(getattr, nave.flight(rf), 'mean_altitude')
+    taxa = 450
+    legs = 250
+    airbrakes = 45000
+
+    rm = runmode()
+
+    nengines = 1 # ONLY FOR IDENTICAL ENGINES   
+    nave.control.brakes = True
+
+    while True:     
+        if altitude() >= taxa:
+            if rm(0):
+                dv = vessel.flight(vessel.orbit.body.reference_frame).speed
+                ########################
+                #Only for 1-engine vessels
+                for eng in vessel.parts.engines:
+                    if eng.active:
+                        Isp=eng.specific_impulse
+                        break
+                ########################
+                mdot = vessel.max_thrust/9.82/Isp #mass flowrate
+                mfoverm0 = exp(-dv/Isp/9.82)
+                dt = vessel.mass*(1-mfoverm0)/mdot
+                ap.target_direction=(0,-1,0)
+                rm+1
+            if rm(1):
+                dir=vessel.direction(vessel.surface_velocity_reference_frame)
+                angle = acos(abs(dir[1]/tnorm(dir)))*(360./(2.*pi))
+                if angle < 1:
+                    rm+1
+            if rm(2):
+                dvvec=vessel.flight(vessel.orbit.body.reference_frame).velocity
+                tcommand = 0.1*tnorm(dvvec)
+                control.throttle = min(max(tcommand,0),1)
+                if tnorm(dvvec)<5:
+                    control.throttle=0
+                    rm+2
+                    continue
+                dir=vessel.direction(vessel.surface_velocity_reference_frame)
+                angle = acos(abs(dir[1]/tnorm(dir)))*(360./(2.*pi))
+                if angle > 5:
+                    control.throttle=0
+                    rm+1
+            if rm(3):
+                dir=vessel.direction(vessel.surface_velocity_reference_frame)
+                angle = acos(abs(dir[1]/tnorm(dir)))*(360./(2.*pi))
+                if angle <2:
+                    rm-1
+            if rm(4):
+                v=vessel.flight(vessel.orbit.body.reference_frame).speed
+                T=0
+                for eng in vessel.parts.engines:
+                    if eng.active:
+                        T=T+eng.available_thrust
+        #                break        
+                g = vessel.orbit.body.surface_gravity
+                m = vessel.mass
+                hburn = v*v/(T/m-g)/2
+                print "{:2.2f} {:2.2f}".format(hburn,vessel.flight(ap.reference_frame).surface_altitude)
+                if vessel.flight(ap.reference_frame).surface_altitude < hburn + 50:
+                    rm+1
+                    control.throttle = 1
+            if rm(5):
+                if vessel.flight(vessel.orbit.body.reference_frame).speed < 7:
+                    rm+1
+            if rm(6):
+                print vessel.flight(vessel.orbit.body.reference_frame).surface_altitude
+                m = vessel.mass
+                v=vessel.flight(vessel.orbit.body.reference_frame).speed
+                tcommand = v-5
+        #        print min(max(0.05*tcommand + m*g/T,0),1)
+                control.throttle = min(max(0.1*tcommand + m*g/T,0),1)
+                if vessel.flight(vessel.orbit.body.reference_frame).surface_altitude < 10:
+                    rm+1
+            if rm(7):                
+                print vessel.flight(vessel.orbit.body.reference_frame).surface_altitude
+                ap.reference_frame = vessel.surface_reference_frame
+                ap.target_direction = (1,0,0)
+                m = vessel.mass
+                v=vessel.flight(vessel.orbit.body.reference_frame).speed
+                tcommand = v-0.5
+                control.throttle = min(max(0.1*tcommand + m*g/T,0),1)
+                if vessel.flight(vessel.orbit.body.reference_frame).surface_altitude < 5:
+                    rm.finish()
+                    control.throttle=0
+                    print ('Landing!')
+
+    control.throttle = 0
+    sleep(10)
+
+    # nave.control.brakes = True
+    # nave.control.rcs = True
+ #    nave.control.sas = True
 
 ## call functions
-launch()
+# launch()
 landing()
 
 ## nao esta chamando as funcoes corretamente, mais perto do que nunca para o suicide burn
