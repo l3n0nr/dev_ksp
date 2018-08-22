fd982b8b2bf1a683f4b8ceaf0e2693117bf9c925
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-08-21 22:06:02 -0300

    arquivos funcionando

--- Python/kerbal/falkinho/modules/base.py
+++ Python/kerbal/falkinho/modules/base.py
@@ -1,33 +1,29 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
-
-# Reference: https://krpc.github.io/krpc/tutorials/launch-into-orbit.html
 
 import math
 import time
 import krpc
+import logging
+import math
 
-conn = krpc.connect(name='Launch into orbit')
-vessel = conn.space_center.active_vessel
-ksc = conn.space_center
-nave = ksc.active_vessel
-rf = nave.orbit.body.reference_frame
-
-# Set up streams for telemetry
-# general
-ut = conn.add_stream(getattr, conn.space_center, 'ut')
-altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
-apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
-stage_2_resources = vessel.resources_in_decouple_stage(stage=2, cumulative=False)
-srb_fuel = conn.add_stream(stage_2_resources.amount, 'SolidFuel')    
-
-# landing
-recursos_estagio_1 = nave.resources_in_decouple_stage(stage=2, cumulative=False)
-combustivel1 = conn.add_stream(recursos_estagio_1.amount, 'LiquidFuel')
-recursos_estagio_2 = nave.resources_in_decouple_stage(stage=0, cumulative=False)
-combustivel2 = conn.add_stream(recursos_estagio_2.amount, 'LiquidFuel')
-
+# Reference: https://krpc.github.io/krpc/tutorials/launch-into-orbit.html
 # profile launch - low orbit
 def launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time):        
+    conn = krpc.connect(name='Launch into orbit')
+    vessel = conn.space_center.active_vessel
+    ksc = conn.space_center
+    nave = ksc.active_vessel
+    rf = nave.orbit.body.reference_frame
+
+    # Set up streams for telemetry
+    # general
+    ut = conn.add_stream(getattr, conn.space_center, 'ut')
+    altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
+    apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+    stage_2_resources = vessel.resources_in_decouple_stage(stage=2, cumulative=False)
+    srb_fuel = conn.add_stream(stage_2_resources.amount, 'SolidFuel')    
+
     print('Systems nominal for launch. T-3 seconds!')
     time.sleep(1)
     print('3...')
@@ -48,13 +44,14 @@ def launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, ma
     vessel.auto_pilot.target_pitch_and_heading(90, 90)  # NORMAL
     
     print('Ignition!')
-
+    
     # Main ascent loop
     srbs_separated = False
     turn_angle = 0
-    while True:      
-        # if altitude() == turn_start_altitude:
-        #     print('----Pitch/Row')
+
+    while True:   
+        if altitude() == turn_start_altitude:
+            print('----Pitch/Row')
 
         # Gravity turn
         if altitude() > turn_start_altitude and altitude() < turn_end_altitude:
@@ -73,16 +70,17 @@ def launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, ma
                 print('----Strongback separated')
                 print('LIFTOOF!')                        
 
-        # # MAX-Q
-        # if altitude() == maxq_begin:
-        #     print('----Max-Q')
+        # MAX-Q
+        if altitude() == maxq_begin:
+            print('----Max-Q')
 
         if altitude() >= maxq_begin and altitude() <= maxq_end:
             vessel.control.throttle = 0.50
         else:
             vessel.control.throttle = 1.0
 
-        if vessel.available_thrust == 0.0:                
+        if vessel.available_thrust == 0.0:     
+            # break
             vessel.control.throttle = 0.30
 
             vessel.control.activate_next_stage()        
@@ -156,7 +154,7 @@ def launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, ma
     vessel.control.throttle = 1.0             
 
     while True:
-        if vessel.available_thrust == 0.0:                
+        if vessel.available_thrust == 0.0:       
             vessel.control.throttle = 0.50
 
             vessel.control.activate_next_stage()        
@@ -186,5 +184,202 @@ def launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, ma
 
     print('Launch complete')
 
+# Reference: 
 def landing():    
-    print('Suicide burn')
+    conn = krpc.connect(name='Suicide Burn')
+    vessel = conn.space_center.active_vessel
+    refer = conn.space_center.active_vessel.orbit.body.reference_frame
+    surAlt = conn.space_center.active_vessel.flight(refer).surface_altitude
+    situacao = conn.add_stream(getattr, vessel, 'situation')
+    pousado_agua = conn.space_center.VesselSituation.splashed
+    pousado = conn.space_center.VesselSituation.landed
+
+    canvas = conn.ui.stock_canvas
+    screen_size = canvas.rect_transform.size
+    panel = canvas.add_panel()
+    rect = panel.rect_transform
+    rect.size = (400, 100)
+    rect.position = (250 - (screen_size[0] / 2), -100)
+    text = panel.add_text("Telemetria")
+    text.rect_transform.position = (-30, 0)
+    text.color = (1, 1, 1)
+    text.size = 16
+
+    global pouso
+    pouso = False    
+
+    ksc = conn.space_center
+    foguete = ksc.active_vessel
+
+    foguete.control.throttle = 0
+    foguete.control.activate_next_stage()  # inicia zerando throttle e ligando motores
+
+    while pouso == False:
+        # Variaveis
+        ksc = conn.space_center
+        foguete = ksc.active_vessel
+        refer = foguete.orbit.body.reference_frame
+        centroEspacial = conn.space_center
+        naveAtual = ksc.active_vessel
+        vooNave = foguete.flight(refer)
+        pontoRef = foguete.orbit.body.reference_frame
+        UT = conn.space_center.ut
+        TWRMax = float()
+        distanciaDaQueima = float()
+        tempoDaQueima = float()
+        acelMax = float()
+        # alturaPouso = 20.0
+        alturaPouso = 50.0
+        speed = float(foguete.flight(refer).speed)
+        altitudeNave = foguete.flight(refer).bedrock_altitude
+        elevacaoTerreno = foguete.flight(refer).elevation
+        massaTotalNave = foguete.mass
+        velVertNave = foguete.flight(refer).vertical_speed
+        piloto = foguete.auto_pilot
+        refer = foguete.orbit.body.reference_frame
+        vooNave = foguete.flight(refer)
+        surAlt = foguete.flight(refer).surface_altitude
+        elevacaoTerreno = foguete.flight(refer).elevation
+        velVertNave = foguete.flight(refer).vertical_speed
+        massa = foguete.mass
+        empuxoMax = foguete.max_thrust
+
+        foguete.control.sas = True
+        vessel.control.rcs = True
+        # foguete.control.sas_mode = foguete.control.sas_mode.retrograde
+        piloto.engage()
+        piloto.target_pitch_and_heading(90, 90)
+
+        naveAtual.control.brakes = True
+        forcaGravidade = foguete.orbit.body.surface_gravity
+        TWRMax = empuxoMax / (massa * forcaGravidade)
+        acelMax = (TWRMax * forcaGravidade) - forcaGravidade
+        tempoDaQueima = speed / acelMax
+        distanciaDaQueima = speed * tempoDaQueima + 1 / 2 * acelMax * pow(tempoDaQueima, 2)
+        distanciaPouso = alturaPouso
+
+        global ultCalculo
+        ultCalculo = 0  # tempo do ultimo calculo
+        global valorEntrada
+        valorEntrada = float(surAlt)
+        global valorSaida
+        valorSaida = float()
+        global valorLimite
+        valorLimite = float(distanciaPouso + distanciaDaQueima)  # variáveis de valores
+
+        global ultValorEntrada
+        ultValorEntrada = float()  # variáveis de cálculo de erro
+
+        global kp
+        kp = float(.022)  # .023
+        global ki
+        ki = float(.001)  # .001
+        global kd
+        kd = float(1)  # 1
+
+        global amostraTempo
+        amostraTempo = 25 / 1000  # tempo de amostragem
+
+        global saidaMin
+        global saidaMax
+        saidaMin = float(-1)
+        saidaMax = float(1)  # limitar saída dos valores
+
+        #
+        global agora
+        global mudancaTempo
+        agora = ksc.ut  # var busca tempo imediato
+        mudancaTempo = agora - ultCalculo  # var compara tempo calculo
+
+        global termoInt
+        termoInt = float()
+
+        def computarPID():
+            global ultCalculo
+            global ultValorEntrada
+            global valorSaida
+            global termoInt
+
+            agora = ksc.ut  # var busca tempo imediato
+            mudancaTempo = agora - ultCalculo  # var compara tempo calculo
+
+            if mudancaTempo >= amostraTempo:  # se a mudança for > q o tempo de amostra, o calculo é feito
+                # var calculo valor saida
+                erro = valorLimite - valorEntrada
+                termoInt += ki * erro
+                if termoInt > saidaMax:
+                    termoInt = saidaMax
+                elif termoInt < saidaMax:
+                    termoInt = saidaMin
+                dvalorEntrada = (valorEntrada - ultValorEntrada)
+                # computando valor saida
+                valorSaida = kp * erro + ki * termoInt - kd * dvalorEntrada
+                if valorSaida > saidaMax:
+                    valorSaida = saidaMax
+                elif valorSaida < saidaMin:
+                    valorSaida = saidaMin
+
+                # relembra valores atuais pra prox
+
+                ultValorEntrada = valorEntrada
+                ultCalculo = agora
+
+            if termoInt > saidaMax:
+                termoInt = saidaMax
+            elif termoInt < saidaMin:
+                termoInt = saidaMin
+            if valorSaida > saidaMax:
+                valorSaida = saidaMax
+            elif valorSaida < saidaMin:
+                valorSaida = saidaMin
+
+            return (valorSaida)
+
+        # Imprimir informacoes
+        print "TWR           : %f" % TWRMax
+        print "Dist. Queima  : %f" % distanciaDaQueima
+        print "Altitude Voo  : %d" % surAlt
+        print "Elev. Terreno : %d" % elevacaoTerreno
+        print "Correcao      : %f" % computarPID()  # esse valor que nao esta atualizando, e deveria atualizar
+
+        novaAcel = 1 / TWRMax + computarPID()  # calculo de aceleracao
+
+        print "Acc Calculada : %f" % novaAcel
+        print "                  "
+
+        text.content = 'Correcao: %f' % computarPID()  # mostra calculo na tela do jogo
+
+        if surAlt < 200:
+            naveAtual.control.gear = True  # altitude para trem de pouso
+
+        if surAlt > 200:
+            naveAtual.control.gear = False
+        if situacao() == pousado or situacao() == pousado_agua:
+            naveAtual.control.throttle = 0
+            pouso = True
+        elif speed <= 6:
+            naveAtual.control.throttle = .1
+        else:
+            naveAtual.control.throttle = novaAcel
+        if speed <= 1:
+            naveAtual.control.throttle = 0
+        #time.sleep(0)
+
+    if situacao() != pousado or situacao() != pousado_agua :
+        main()
+    else:
+        print("Foguete atualmente pousado")
+        print("Encerrando processo...")
+    text.content = 'Foguete Pousado'
+    naveAtual.control.throttle = 0
+    vessel.control.rcs = True
+    vessel.control.sas = True
+    # vessel.control.sas_mode = conn.space_center.SASMode.stability_assist
+    print('TOUCHDOWN!!!!!')
+    time.sleep(2)
+    print('estabilizando')
+    time.sleep(6)
+    print('pouso terminado, desligando tudo, tchau!!')
+    vessel.control.sas = False
+    vessel.control.rcs = False
+    vessel.control.brakes = False
--- Python/kerbal/falkinho/modules/orbita.py
+++ Python/kerbal/falkinho/modules/orbita.py
@@ -4,12 +4,12 @@
 from base import launch
 
 # variables default
-turn_start_altitude     = 1000
+turn_start_altitude     = 200
 turn_end_altitude       = 45000
 target_altitude         = 140000
-maxq_begin              = 25000
+maxq_begin              = 30000
 maxq_end                = 70000
-correction_time         = 0.1
+correction_time         = 1
 
 # start script
 launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time)
