3424093333f80f381d53a93593d66b0287c8460c
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-08-30 21:41:48 -0300

    check value

--- Python/kerbal/calculate_fuel.py
+++ Python/kerbal/calculate_fuel.py
@@ -1,40 +1,50 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+plus_less=0.50  # percent
 mass=14948      # 14.948 kg, final stage
 taxa=0.145      # 0.145% fuel, first stage
+mass_30plus=mass+(mass*plus_less)
+mass_30less=mass-(mass*plus_less)
 
-value=1135
-                # x = ((24948*0,145)/14948) - 0,145 = 0,097002944                                
-                # x = ((5135*0,145)/14948) + 0,145  = 0,194811012
-                #   x = ((1948*0,145)/14948)- 0,145 = −0,126103827
-                # x = ((14948*0,145)/14948)         = 0,145
+value=14948
+    # Test values:
+    # x = ((24948*0,145)/14948) - 0,145 = 0,097002944                                
+    # x = ((5135*0,145)/14948) + 0,145  = 0,194811012
+    #   x = ((1948*0,145)/14948)- 0,145 = −0,126103827
+    # x = ((14948*0,145)/14948)         = 0,145
 
-def calc(mass, taxa, value):
+def calc(mass, taxa, value):    
+    x = ((value * taxa)/mass)
+
+    # print x
+
     if value > mass:
-        x = ((value * taxa)/mass) 
-        x = x - taxa
-
-        print ('value:',value, '>', 'mass:',mass)      
-    elif value < mass:        
-        # x = ((value * taxa)/mass)   
-        x = ((value / mass)+taxa)   
-        # x = taxa + x
-
-        print ('mass:',mass, '>','value:',value)      
+        if x <= 0.99:
+            print ('Check you probe, this is very heavy!!')            
+            # print x
+        else:
+            x = x - taxa
+            print 'Fuel first stage:',x,'%'
+            # print ('value:',value, '>', 'mass:',mass)      
+    elif value < mass:          
+        x = ((value * taxa)/mass)
+        if x <= 0.02:
+            print ('Check you probe, this is very light!!')            
+            # print x
+        else:
+            x = x + taxa
+            print 'Fuel first stage:',x,'%'
+            # print ('mass:',mass, '>','value:',value)      
     else:
         x = ((value * taxa)/mass)
+        print 'Fuel first stage:',x,'%'
+        # print ('value = mass')          
 
-        print ('value = mass')      
-    
-    print x
-    
-    # if x <= 0.10:
-    #     print ('Heavy-heavy!!')
-    # elif x > 0.18:
-    #     print ('Light-light!!')
-    # else:
-    #     print ('Lets GO!!')
-
-
-calc(mass, taxa, value)
+if mass_30less >= value or mass_30plus <= value or value == mass:
+    calc(mass, taxa, value)
+else:
+    print '+',mass_30plus
+    print '-',mass_30less
+    print 'V',value
+    print ('Very heavy or very light. Check you vessel!')
