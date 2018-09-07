eb9bdf1951796735c6d227d9e67ba36c3f0ad0c8
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-07 11:06:24 -0300

    file improviment

--- Python/real/calculate_seconds.py
+++ Python/real/calculate_seconds.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import os
 
-# input time
-# Example 
-#		040506 = 04h 05m 06s
-value = raw_input("Time: ")	
+def calculate():
+    value = raw_input("Time[HH:MM:SS]: ")       # 040506 = 04h 05m 06s
 
-# convert numbers
-hours = int(value[0:2])
-minute = int(value[2:4])
-second = int(value[4:6])
+    # convert numbers
+    hours = int(value[0:2])
+    minute = int(value[2:4])
+    second = int(value[4:6])
 
-hours_conv = (hours * 3600)
-minute_conv = (minute * 60)
-second_conv = (hours_conv + minute_conv + second)
+    hours_conv = (hours * 3600)
+    minute_conv = (minute * 60)
+    second_conv = (hours_conv + minute_conv + second)
 
-print second_conv, "seconds"
+    print second_conv, "seconds"
+
+def main():
+    calculate()
+
+# call main function
+main()
