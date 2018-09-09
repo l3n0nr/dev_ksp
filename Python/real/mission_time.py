1341c0cf021c07bd110839b4d013e87d100ce15b
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-09 20:07:50 -0300

    check time

--- Python/real/mission_time.py
+++ Python/real/mission_time.py
@@ -3,7 +3,7 @@
 ###############################################
 # AUTHOR: lenonr
 #
-# VERSION: 0.10
+# VERSION: 0.25
 #
 # CREATION DATE: 09/09/18
 # LAST MODIFICATION: 09/09/18
@@ -12,8 +12,8 @@
 #   Show time profile by type rocket, one by one using local time.
 #
 # REFERENCE:
-#	<https://www.tutorialspoint.com/python/python_date_time.htm>
-#	<https://pymotw.com/2/datetime/>
+#   <https://www.tutorialspoint.com/python/python_date_time.htm>
+#   <https://pymotw.com/2/datetime/>
 #
 ###############################################
 # BODY
@@ -25,9 +25,6 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 # VARIABLES
 var_sleep=0     # 0 = disabled # 1 = enable
-time_hours   = 0
-time_minutes = 0
-time_seconds = 0
 
 ## functions
 sequence_f9_land = [ "LIFTOFF", "Max-Q", "MECO", 
@@ -43,7 +40,7 @@ countdown_f9_land = [ "000000", "000107", "000233",
                       "003206" ]
 
 ## profile mission
-def f9_land():
+def f9_land(launch_time, hour_launch, minute_launch, second_launch):    
     print ("SEQUENCE STARTED!")
     print ('#' * 20)            
 
@@ -77,6 +74,23 @@ def f9_land():
 
             next_action = second_now_conv_x - second_now_conv
 
+            if x == 0:
+                print "Clock time:",hour_launch,":",minute_launch,":",second_launch
+            else:
+                sum_hour    = int(hour_launch) + int(hours_x)
+                sum_min     = int(minute_launch) + int(minutes_x)
+                sum_second  = int(second_launch) + int(seconds_x)
+
+                if sum_second > 59:
+                    sum_second = sum_second - 60
+                    sum_min = sum_min + 1
+
+                if sum_min > 59:
+                    sum_min = sum_min - 60
+                    sum_hour = sum_hour + 1
+
+                print "Clock time:",sum_hour,":",sum_min,":", sum_second
+
             print "T+", hours, "h", minutes, "m", seconds, "s", " |", sequence_f9_land[x]
             print "* Next action: " , hours_x, "h", minutes_x, "m", seconds_x, "s", "-", sequence_f9_land[x+1], "\n"
 
@@ -84,46 +98,50 @@ def f9_land():
                 time.sleep(next_action)
 
 def test_date():
-	format="%H:%M:%S" 
-	date_now = datetime.strftime(datetime.now(),format)
+    format="%H:%M:%S" 
+    date_now = datetime.strftime(datetime.now(),format)    
 
-	# print date_now	
+    hour_now        = date_now[0:2]
+    minute_now      = date_now[3:5]
+    second_now      = date_now[6:8]
 
-	# t = datetime.time(1, 2, 3)
-	# hour_local 	 = t.hour
-	# minute_local = t.minute
-	# second_local = t.second
+    print hour_now
+    print minute_now
+    print second_now
 
-	# print hour_local
-	# print minute_local
-	# print second_local
+    # launch_time = raw_input("\nLaunch in:")
 
-	launch_time = raw_input("\nLaunch in:")
-	hour_launch = launch_time[0:2]
-	minute_launch = launch_time[2:4]
-	second_launch = launch_time[4:6]
+    launch_time='123003'
 
-	date_launch = hour_launch,minute_launch,second_launch
+    hour_launch = launch_time[0:2]
+    minute_launch = launch_time[2:4]
+    second_launch = launch_time[4:6]
 
-	print "Date now:",date_now
-	print "Date for launch:",date_launch
+    print hour_launch
+    print minute_launch
+    print second_launch
 
-	# print hour_launch
-	# print minute_launch
-	# print second_launch
+    sum_hour    = int(hour_now) + int(hour_launch)
+    sum_min     = int(minute_now) + int(minute_launch)
+    sum_second  = int(second_now) + int(second_launch)
 
-
+    print sum_hour
+    print sum_min
+    print sum_second
 
 def main():
-    # rocket_name = raw_input("\nThe time for launch: ")           
+    # launch()
+    # launch_time = raw_input("\nLaunch in:")
 
-    # time_hours   = rocket_name[0:2]
-    # time_minutes = rocket_name[2:4]
-    # time_seconds = rocket_name[4:6]    
+    launch_time = "123800"
 
-    # f9_land()
+    hour_launch = launch_time[0:2]
+    minute_launch = launch_time[2:4]
+    second_launch = launch_time[4:6]
 
-    test_date()
+    f9_land(launch_time, hour_launch, minute_launch, second_launch)
+
+    # test_date()
 
 main()
 
