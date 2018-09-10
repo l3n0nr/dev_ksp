aae139735c0cf6e1e07d04cdc4f4dbe33d2d504b
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-10 00:01:43 -0300

    parametros ok

--- Python/kerbal/falkinho/v3/triplo.py
+++ Python/kerbal/falkinho/v3/triplo.py
@@ -8,10 +8,10 @@ from base import suborbital_triplo
 def main():
 	turn_start_altitude     = 1000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
-	target_altitude         = 1000000					# target 
+	target_altitude         = 1500000					# target 
 	maxq_begin              = 25000						# reduce aceleration stage - begin
 	maxq_end                = 70000						# reduce aceleration stage - end
-	taxa					= 0.15						# Landing first stage in %(Taxa/100)
+	taxa					= 0.16						# Landing first stage in %(Taxa/100)
 	orientation				= 90						# NORMAL
 
 	suborbital_triplo(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
