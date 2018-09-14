de185f933e44a05acf667015058df78b772d9d68
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-14 14:12:52 -0300

    info add

--- Python/kerbal/circularize.py
+++ Python/kerbal/circularize.py
@@ -3,7 +3,7 @@
 ## import modules
 from base import circularize
 
-target_altitude = 1797000.41
+target_altitude = 35794000
 	# KERBIN = KM
 		# GEO-EQUA 		= 2863000.33
 			# Orbit period: 04 hours - 03 satellites
@@ -11,5 +11,8 @@ target_altitude = 1797000.41
 	# MUN = KM
 		# GEO-EQUA  	= 2970000.608
 		# SEMI-SINCRO	= 1797000.41
+
+	# MINMUS = KM	
+		# GEO 			= 35794000 
 
 circularize(target_altitude)
