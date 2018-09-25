c584a8435c64f238970570ebdb6022fea02f973c
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-25 18:45:08 -0300

    ship improvement

--- Python/kerbal/falkinho/v3/dragao.py
+++ Python/kerbal/falkinho/v3/dragao.py
@@ -11,17 +11,8 @@ def main():
 	target_altitude         = 150000					# target 
 	maxq_begin              = 25000						# reduce aceleration stage - begin
 	maxq_end                = 70000						# reduce aceleration stage - end
-	taxa					= 0.125						# dragao
+	taxa					= 0.12						# dragao
 	orientation				= 90						# NORMAL
-
-			# 45  : TOP-NORMAL							# 1.5 hours in the clock.
-			# 90  : NORMAL 								# 3 hours in the clock.
-			# 135 : NORMAL-DOWN							# 4.5 hours in the clock.
-			# 180 : DOWN 								# 6 hours in the clock.
-			# 225 : DOWN-ANTINORMAL						# 7.5 hours in the clock.
-			# 270 : ANTINORMAL 							# 9 hours in the clock.
-			# 315 : ANTINORMAL-TOP						# 10.5 in the clock.
-			# 360 : TOP									# 12 hours in the clock.
 
 	suborbital(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
 
