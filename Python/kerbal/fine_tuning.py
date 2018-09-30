639e4bd9c5d557e2f74acb48e74d463b02d8ebe4
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-29 21:04:26 -0300

    files moved

--- Python/kerbal/fine_tuning.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-## import modules
-from base import fine
-
-correction_time = 1
-
-fine(correction_time)
--- Python/kerbal/orbital_maneuver.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-## import modules
-from base import orbital_maneuver
-
-orbital_maneuver()
