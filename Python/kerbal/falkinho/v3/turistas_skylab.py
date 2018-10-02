f99d9297da67707a0d0408091d3354e2e3e38a70
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-02 14:20:08 -0300

    file improvement

--- Python/kerbal/falkinho/v3/turistas_skylab.py
+++ Python/kerbal/falkinho/v3/turistas_skylab.py
@@ -8,14 +8,14 @@ from base import suborbital
 def main():
 	turn_start_altitude     = 1000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
-	target_altitude         = 150000					# target 		-	VERIFICAR
+	target_altitude         = 180000					# target 
 	maxq_begin              = 25000						# reduce aceleration stage - begin
 	maxq_end                = 70000						# reduce aceleration stage - end
 	
 	# taxa					= 0.16						# progress
 	# taxa					= 0.19						# turistas	/ rebocador
-	taxa					= 0.10						# turistas	/ laboratorio - salyut
-	taxa					= 0.15						# turistas	/ laboratorio - skylab
+	# taxa					= 0.10						# turistas	/ laboratorio - salyut
+	taxa					= 0.13						# turistas	/ laboratorio - skylab
 
 	# orientation				= 41.7						# TOP-NORMAL
 	orientation				= 135						# DOWN-NORMAL	- TRANSFER WINDOW
