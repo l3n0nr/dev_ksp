ed98a2a211ef38fd8e5b024531254abde89ad7de
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-15 22:02:25 -0300

    profile landing zone add - spacex style

---
+++ Python/kerbal/falkinho/v3/landing_zone.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../../')
+from base import landing_zone
+
+def main():	
+	target_altitude         = 180000					# target 
+	maxq_begin              = 25000						# reduce aceleration stage - begin
+	maxq_end                = 36000						# reduce aceleration stage - end
+	turn_start_altitude     = 1000						# inclination begin
+	turn_end_altitude       = target_altitude			# inclination end
+	
+	taxa					= 0.18						# dragao NR
+
+	orientation				= 90						# NORMAL
+
+	landing_zone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
+
+main()
