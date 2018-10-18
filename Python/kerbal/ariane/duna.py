ebec329e56943dacad630289313aa57c3eb0c686
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-18 17:47:06 -0300

    profile improvement

--- Python/kerbal/ariane/duna.py
+++ Python/kerbal/ariane/duna.py
@@ -9,7 +9,7 @@ def main():
 	turn_start_altitude     = 2000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
 	target_altitude         = 120000					# target 
-	maxq_begin              = 25000						# reduce aceleration stage - begin
+	maxq_begin              = 35000						# reduce aceleration stage - begin
 	maxq_end                = 36000						# reduce aceleration stage - end
 	correction_time			= 1							# fine tuning - orbital manuever
 	orientation				= 90						# NORMAL
