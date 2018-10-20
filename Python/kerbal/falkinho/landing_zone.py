6428eab66493e8bb904962f7364110277a1429ca
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-20 01:18:58 -0300

    check this file

--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -6,13 +6,24 @@ sys.path.insert(0, '../')
 from base import landing_zone
 
 def main():	
-	target_altitude         = 180000					# target 
-	maxq_begin              = 25000						# reduce aceleration stage - begin
+	# target_altitude         = 180000					# target 
+	target_altitude         = 150000						# target 
+
+	maxq_begin              = 35000						# reduce aceleration stage - begin	
 	maxq_end                = 36000						# reduce aceleration stage - end
+
 	turn_start_altitude     = 100						# inclination begin
-	turn_end_altitude       = target_altitude			# inclination end
+
+	if (target_altitude/2) > 45000:
+		turn_end_altitude = 45000
+	else:
+		turn_end_altitude = (target_altitude/2)
+
+	# turn_end_altitude       = (target_altitude/2)			# inclination end
+	# turn_end_altitude       = 45000						# inclination end
 	
-	taxa					= 0.15						# dragao NR
+	# taxa					= 0.15						# dragao  			- 		space station
+	taxa					= 0.25						# dragao  			-		docking to manuveur for mun
 
 	orientation				= 90						# NORMAL
 
