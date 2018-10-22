efc48e01e14f1fd55f91eb637b33b88eab4350a3
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-21 23:16:32 -0300

    file improvement - check later

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -6,6 +6,12 @@ sys.path.insert(0, '../')
 from base import boostback
 
 def main():
-	boostback()
+	# value			=	-80			# dragao
+	value			=	-25			# lander mun
+
+	# v2  = -80             # dragao
+    # v2  = -25             # lander mun
+
+	boostback(value)
 
 main()
