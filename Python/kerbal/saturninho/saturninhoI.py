def0389433eddef4ef81f27a0f54e27315413111
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-31 19:27:02 -0300

    profile saturninho improvement

--- Python/kerbal/saturninho/saturninhoI.py
+++ Python/kerbal/saturninho/saturninhoI.py
@@ -3,7 +3,7 @@
 ## import modules
 import sys
 sys.path.insert(0, '../')
-from base import launch
+from base import saturninho
 
 turn_start_altitude     = 1000						# inclination begin
 turn_end_altitude       = 45000						# inclination end
@@ -14,4 +14,4 @@ correction_time         = 1							# time correction before shutdown engine
 taxa					= 0							# Landing first stage in %(Taxa/100)
 orientation				= 90						# 90 - NORMAL | 180 - DOWN | 270 - ANTINORMAL | 360 - TOP
 
-launch(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, taxa, orientation)
+saturninho(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, taxa, orientation)
