443d5891fdce773ad6be9b1be4be26b7b97fd7a7
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-31 22:18:04 -0300

    profile add

---
+++ Python/kerbal/ariane/mun.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import ariane
+
+def main():
+	turn_start_altitude     = 2000						# inclination begin
+	turn_end_altitude       = 45000						# inclination end	
+	maxq_begin              = 34000						# reduce aceleration stage - begin
+	maxq_end                = 36000						# reduce aceleration stage - end
+	correction_time			= 1							# fine tuning - orbital manuever
+	orientation				= 90						# NORMAL
+
+	target_altitude         = 120000					# 5 turistas + satelite
+
+	ariane(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, orientation)
+
+main()
