69f32ca49d6e51ae0ed7c4cd30899a3b6da08fcd
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-05 17:10:14 -0200

    profile add, check this

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -6,8 +6,10 @@ sys.path.insert(0, '../')
 from base import boostback
 
 def main():
-	value			=	-80			# dragao
-	# value			=	-25			# lander mun
+	# value			=	-80			# dragao
+	# value			=	-25			# lander mun v1
+	value			=	-25			# lander mun v2 		## CHECK THIS!
+
 	# value			=	-70			# sat-climate I
 	# value			=	-95			# turistas station
 	# value			=	-80			# sat-climate I
