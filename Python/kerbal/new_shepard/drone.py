996aaf579d63a80b9ecdb903dc636a715af7bff8
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-05 09:52:33 -0200

    profile improvement

--- Python/kerbal/new_shepard/drone.py
+++ Python/kerbal/new_shepard/drone.py
@@ -10,10 +10,9 @@ def main():
 	turn_start_altitude     = 3000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
 	target_altitude         = 180000					# target 
-	maxq_begin              = 35000						# reduce aceleration stage - begin
+	maxq_begin              = 36000						# reduce aceleration stage - begin
 	maxq_end                = 70000						# reduce aceleration stage - end
 	taxa					= 0.10						# landing new shepard
-
 	orientation				= 3							# TOP
 
 	new_shepard(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
