4b79bb2750cf3d0721691ded9c8fb660b8443d65
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-11 14:35:13 -0200

    profile improviment

--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -18,7 +18,8 @@ def main():
 	# taxa					= 0.22				# turistas station	-	+/- 05.650 kg
 	# taxa					= 0.22				# lander mun v2		-	+/- 15.000 kg
 	# taxa					= 0.24				# deorbit I			-	+/- 04.345 kg
-	taxa					= 0.26				# science I + HUB	-	+/- 1.820 kg
+	# taxa					= 0.26				# science I + HUB	-	+/- 01.820 kg
+	taxa					= 0.24				# 2 hub's station	-	+/- 04.550 kg
 
 	orientation				= 90
 
