e1e7c834b2320645607cebf0b7057a94e1d9f34a
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-11 14:05:47 -0200

    profile impŕoviment

--- Python/kerbal/falkinho/landing.py
+++ Python/kerbal/falkinho/landing.py
@@ -8,13 +8,8 @@ def main():
 	alturaPouso					=			35				# DEFAULT
 	engines_landing				=			3				# 3/8 engines in landing 		
 	altitude_landing_burn		=			1500			# altitude for shutdown unnecessary engines
-	deploy_legs					=			45				# deploy landing legs
+	deploy_legs					=			40				# deploy landing legs
 
-	# profile						=			"Falkinho"		# profile landing
-	
-	sound 						= 			True 			# sound script landing
-
-	# landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, profile, sound)
-	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Falkinho", sound)
+	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Falkinho", True)
 
 main()
