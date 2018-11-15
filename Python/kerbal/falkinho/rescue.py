9e50d77810c5f3d8747d8fa0bf43397e212c5e81
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-14 22:59:06 -0200

    profiles improviments

---
+++ Python/kerbal/falkinho/rescue.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import falkinho
+
+def main():
+	######################################################################################
+	turn_start_altitude     = 1000						# inclination begin
+	turn_end_altitude       = 45000						# inclination end
+	target_altitude         = 180000					# target 
+	maxq_begin              = 25000						# reduce aceleration stage - begin
+	maxq_end                = 70000						# reduce aceleration stage - end	
+	######################################################################################
+
+	orientation			  	= 90						# normal
+
+	# taxa					= 0.19						# turistas	/ rebocador
+	# taxa					= 0.18						# turistas 12	
+	# taxa					= 0.155						# abastecimento
+	taxa					= 0.12						# landing island
+
+	falkinho(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
+	
+main()
