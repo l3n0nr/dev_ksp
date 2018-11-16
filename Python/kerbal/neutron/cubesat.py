7468e3c0b35abae2a48f54cbc10a10663d8dcb40
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-16 10:17:22 -0200

    profile neutron improviment

--- Python/kerbal/neutron/cubesat.py
+++ Python/kerbal/neutron/cubesat.py
@@ -8,13 +8,23 @@ from base import neutron
 def main():
 	turn_start_altitude     = 1000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
-	target_altitude         = 130000					# target 
 	maxq_begin              = 30000						# reduce aceleration stage - begin
 	maxq_end                = turn_end_altitude			# reduce aceleration stage - end
 	correction_time			= 1							# fine tuning - orbital manuever
+
+	target_altitude         = 130000					# target
 	
-	orientation				= 41.7						# TOP-NORMAL
-	# orientation				= 135						# DOWN-NORMAL
+	orientation				= 135						# inclination 01
+	# orientation				= 180						# inclination 02
+
+	######## IDEAL ########
+	## inclination 1	 
+	## inclination 2
+	## inclination 1	 
+	## inclination 2
+	## inclination 1	 
+	## inclination 2
+	######## 
 
 	neutron(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, orientation)
 
