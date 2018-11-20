bc9680e452ee45c00752f2b7c9a10e96512a8c71
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-20 20:08:41 -0200

    profile ns capsula add

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -6,15 +6,19 @@ sys.path.insert(0, '../')
 from base import boostback
 
 def main():
+	#########################################################################
 	# value			=	-80			# dragao				-	+/- 24.000 kg
 	# value			=	-25			# lander mun v1			-	+/- 19.000 kg
 	# value			=	-70			# sat-climate I 		-	+/- 02.500 kg
 	# value			=	-95			# turistas station 		-	+/- 05.650 kg
 	# value			=	-80			# sat-climate II		- 	+/- 02.500 kg
 	# value			=	-80			# lander mun v2			-	+/- 15.000 kg
-	value			=	-78			# deorbit I 			-	+/- 04.430 kg
+	# value			=	-78			# deorbit I 			-	+/- 04.430 kg
 	# value			=	-65			# science I + HUB		-	+/- 01.820 kg
 	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg
+	#########################################################################
+
+	value			=	-85			# ns capsula 			-	+/- 04.550 kg			????
 
 	boostback(value)
 
--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -6,22 +6,24 @@ sys.path.insert(0, '../')
 from base import falkinho_landing_zone
 
 def main():	
+	#################################################################################
 	target_altitude         = 180000					
 	turn_start_altitude     = 2000						
 	turn_end_altitude       = (target_altitude/1.5)		
 	maxq_begin				= 28000
 	maxq_end				= 30000
-	
+	orientation				= 90
+	#################################################################################
 	# taxa					= 0.19				# dragao			-	+/- 24.000 kg
 	# taxa					= 0.19				# lander mun v1		-	+/- 19.000 kg
 	# taxa					= 0.25				# sat-climate		-	+/- 02.500 kg
 	# taxa					= 0.22				# turistas station	-	+/- 05.650 kg
 	# taxa					= 0.22				# lander mun v2		-	+/- 15.000 kg
-	taxa					= 0.24				# deorbit I			-	+/- 04.345 kg
+	# taxa					= 0.24				# deorbit 			-	+/- 04.345 kg
 	# taxa					= 0.26				# science I + HUB	-	+/- 01.820 kg
 	# taxa					= 0.24				# 2 hub's station	-	+/- 04.550 kg
 
-	orientation				= 90
+	taxa					= 0.22				# ns capsula		-	+/- 10.105 kg			????
 
 	falkinho_landing_zone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
 
