f94f408dba63081c2b9b825e326b9149cb0bb5e6
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-22 15:39:59 -0200

    profile improviment

--- Python/kerbal/velorg/sat_geo.py
+++ Python/kerbal/velorg/sat_geo.py
@@ -9,13 +9,11 @@ def main():
 	########################################################################################
 	turn_start_altitude     = 16000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
-	target_altitude         = 90000						# target 
+	target_altitude         = 100000					# target 
 	maxq_begin              = 22000						# reduce aceleration stage - begin
 	maxq_end                = 36000						# reduce aceleration stage - end
-	correction_time			= 1							# fine tuning - orbital manuever
-	orientation				= 90						# NORMAL
 	########################################################################################
 
-	velorg(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, orientation)
+	velorg(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, 1, 90)
 
 main()
