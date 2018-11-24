15e671e13c712f87c565da5ad24e39983d2035e2
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-24 17:36:20 -0200

    profile add

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -16,9 +16,10 @@ def main():
 	# value			=	-78			# deorbit I 			-	+/- 04.430 kg
 	# value			=	-65			# science I + HUB		-	+/- 01.820 kg
 	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg
+	# value			=	-85			# ns capsula TR 		-	+/- 10.105 kg
 	#########################################################################
 
-	value			=	-85			# ns capsula 			-	+/- 04.550 kg			????
+	value			=	-95			# ns capsula NTR		-	+/- 10.105 kg
 
 	boostback(value)
 
--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -14,6 +14,7 @@ def main():
 	maxq_end				= 30000
 	orientation				= 90
 	#################################################################################
+	
 	# taxa					= 0.19				# dragao			-	+/- 24.000 kg
 	# taxa					= 0.19				# lander mun v1		-	+/- 19.000 kg
 	# taxa					= 0.25				# sat-climate		-	+/- 02.500 kg
@@ -22,8 +23,9 @@ def main():
 	# taxa					= 0.24				# deorbit 			-	+/- 04.345 kg
 	# taxa					= 0.26				# science I + HUB	-	+/- 01.820 kg
 	# taxa					= 0.24				# 2 hub's station	-	+/- 04.550 kg
+	# taxa					= 0.22				# ns capsula TR		-	+/- 10.105 kg
 
-	taxa					= 0.22				# ns capsula		-	+/- 10.105 kg			????
+	taxa					= 0.22				# ns capsula NTR	-	+/- 10.105 kg
 
 	falkinho_landing_zone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
 
