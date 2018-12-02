a00968ad8b312ad0c7b9ede28425ee1d0a9ab84f
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-02 00:52:06 -0200

    profile improviment

--- Python/kerbal/falkinho/landing.py
+++ Python/kerbal/falkinho/landing.py
@@ -7,8 +7,8 @@ from base import landing_advanced
 def main():
 	alturaPouso					=			35				# DEFAULT	
 	engines_landing				=			4				# 4/8 engines in landing 		
-	altitude_landing_burn		=			1200			# altitude for shutdown unnecessary engines
-	deploy_legs					=			40				# deploy landing legs
+	altitude_landing_burn		=			2000			# altitude for shutdown unnecessary engines
+	deploy_legs					=			100				# deploy landing legs
 
 	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Falkinho", True)
 
