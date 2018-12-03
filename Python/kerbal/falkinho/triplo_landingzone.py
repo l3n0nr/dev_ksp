05372ca85eb8e458c4b0398cd730b3015e1e78a5
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-03 21:43:26 -0200

    profile falkinho_triplo improviment

--- Python/kerbal/falkinho/triplo_landingzone.py
+++ Python/kerbal/falkinho/triplo_landingzone.py
@@ -10,13 +10,15 @@ def main():
     turn_start_altitude     = 2000                               
     maxq_begin              = 28000
     maxq_end                = 32000
-    taxa_meco               = 0
+    # taxa_meco               = 0
     #####################################################################
 
-    target_altitude         = 320000    
+    target_altitude         = 300000    
     turn_end_altitude       = (target_altitude/1.5)
-    taxa_beco               = 0.18                                  # separation boosters
 
-    falkinho_triplo_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa_beco, taxa_meco, 90)
+    taxa_meco               = 0.18
+
+    # falkinho_triplo_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa_beco, taxa_meco, 90)
+    falkinho_triplo_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa_meco, 90)
 
 main()
