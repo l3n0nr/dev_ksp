83c97b5ce258e87f04023ea38fab427dd347207e
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-06 11:48:39 -0200

    profile add

--- Python/kerbal/ariane/satellites.py
+++ Python/kerbal/ariane/satellites.py
@@ -10,15 +10,20 @@ def main():
 	turn_start_altitude     = 2000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end	
 	maxq_begin              = 34000						# reduce aceleration stage - begin
-	maxq_end                = 36000						# reduce aceleration stage - end
-	
-	# orientation				= 90						# NORMAL
-	orientation				= 155						# DOWN(180) - 25
-
+	maxq_end                = 36000						# reduce aceleration stage - end	
 	#######################################################################################
 
-	# target_altitude         = 120000					# 1 satellite mission + 1 satellite geo
-	target_altitude         = 100000					# Sat Tundra I
+	# 1 satellite mission + 1 satellite geo		-		NORMAL
+		# orientation				= 90				
+		# target_altitude         = 120000
+
+	# Sat Tundra I 								-		DOWN(180-25) 
+		# orientation				= 155					
+		# target_altitude         = 100000					
+	
+	# Surveilance Camera II 					- 		DOWN
+	orientation					=	180
+	target_altitude				=	120000
 
 	ariane(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, 1, orientation)
 
