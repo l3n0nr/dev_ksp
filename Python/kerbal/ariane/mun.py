7ea7c3248c2057a55d6c77885e0394756c377021
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-12 19:37:34 -0200

    profile explorer I add

--- Python/kerbal/ariane/mun.py
+++ Python/kerbal/ariane/mun.py
@@ -6,15 +6,17 @@ sys.path.insert(0, '../')
 from base import ariane
 
 def main():
+	#######################################################################################
 	turn_start_altitude     = 2000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end	
 	maxq_begin              = 34000						# reduce aceleration stage - begin
 	maxq_end                = 36000						# reduce aceleration stage - end
-	correction_time			= 1							# fine tuning - orbital manuever
-	orientation				= 90						# NORMAL
+	#######################################################################################	
 
-	target_altitude         = 120000					# 5 turistas + satelite
+	# Ariane II
+	# target_altitude         = 120000					# 5 turistas + satelite
+	target_altitude         = 120000					# Explorer I 
 
-	ariane(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, correction_time, orientation)
+	ariane(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, 1, 90)
 
 main()
