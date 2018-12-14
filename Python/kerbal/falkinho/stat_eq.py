9d78bff6d810bd608059002eb31758754ebb6d2a
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-14 20:39:17 -0200

    files improviments

--- Python/kerbal/falkinho/stat_eq.py
+++ Python/kerbal/falkinho/stat_eq.py
@@ -9,14 +9,14 @@ def main():
 	######################################################################################
 	turn_start_altitude     = 1000						# inclination begin
 	turn_end_altitude       = 45000						# inclination end
-	target_altitude         = 180000					# target 
+	target_altitude         = 180000					# target orbit
 	maxq_begin              = 25000						# reduce aceleration stage - begin
 	maxq_end                = 70000						# reduce aceleration stage - end
 	######################################################################################
 
 	# taxa					= 0.19						# turistas	/ rebocador
-	# taxa					= 0.18						# turistas 12	
-	taxa					= 0.155						# abastecimento
+	taxa					= 0.18						# turistas 12	
+	# taxa					= 0.155						# abastecimento
 
 	falkinho(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, 90)
 	
