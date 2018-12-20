12d8813b0ad27cb5f4715b5d6dc43e9691a0e7f8
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-19 22:05:40 -0200

    profile improviment

--- Python/kerbal/new_gleen/landing.py
+++ Python/kerbal/new_gleen/landing.py
@@ -7,13 +7,12 @@ from base import landing_advanced
 def main():
 	#######################################################################################
 	alturaPouso					=			30				# default
-	engines_landing				=			7				# engines in landing
-	altitude_landing_burn		=			2000			# altitude for shutdown engines
+	engines_landing				=			7				# 7 engines in landing
+		## Config ship in KSP
+	altitude_landing_burn		=			1500			# altitude for shutdown engines
 	deploy_legs					=			100				# deploy landing legs
-	profile						=			"New Glenn"		# profile landing
-	sound 						= 			False 			# sound script landing	
 	#######################################################################################
 
-	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, profile, sound)
+	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "New Gleen", False)
 
 main()
