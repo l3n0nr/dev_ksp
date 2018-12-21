0461a4da7878041159f51b91770b920011368160
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-21 11:06:27 -0200

    profile improviment

--- Python/kerbal/new_gleen/landing.py
+++ Python/kerbal/new_gleen/landing.py
@@ -6,9 +6,8 @@ from base import landing_advanced
 
 def main():
 	#######################################################################################
-	alturaPouso					=			30				# default
+	alturaPouso					=			35				# default
 	engines_landing				=			7				# 7 engines in landing
-		## Config ship in KSP
 	altitude_landing_burn		=			1500			# altitude for shutdown engines
 	deploy_legs					=			100				# deploy landing legs
 	#######################################################################################
