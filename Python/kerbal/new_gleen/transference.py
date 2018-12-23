c4fc86d5dddf4f537f82d3b1206c407dcce3be8f
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-23 11:42:59 -0200

    profile new gleen improviment

---
+++ Python/kerbal/new_gleen/transference.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import newgleen
+
+def main():
+	######################################################################################
+	turn_start_altitude     = 1000						# inclination begin
+	turn_end_altitude       = 45000						# inclination end
+	target_altitude         = 180000					# target orbit
+	maxq_begin              = 25000						# reduce aceleration stage - begin
+	maxq_end                = 70000						# reduce aceleration stage - end
+	######################################################################################
+	#
+	#################################################################################
+	#		X					Value				Profile					Weight	#
+	#################################################################################
+	# PROFILE MISSIONS
+	taxa					= 	0.05				# Transference			31.500 kg
+	#
+	#################################################################################
+
+	newgleen(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, 90)
+	
+main()
