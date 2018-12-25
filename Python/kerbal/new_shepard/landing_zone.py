caf3c0d50194756a8b70e7bfc58d5c39cd51ec80
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-12-25 09:25:55 -0200

    profile improviment

--- Python/kerbal/new_shepard/landing_zone.py
+++ Python/kerbal/new_shepard/landing_zone.py
@@ -7,14 +7,13 @@ from base import newshepard_landingzone
 
 def main():	
 	#################################################################################
+	#
 	target_altitude         = 180000					
 	turn_start_altitude     = 2000						
 	turn_end_altitude       = (target_altitude/2)	
-
 	maxq_begin				= 28000
 	maxq_end				= 30000
-	orientation				= 90	
-
+	#
 	#################################################################################
 	#		X					Value				Profile					Weight	#
 	#################################################################################
@@ -24,6 +23,6 @@ def main():
 	#################################################################################
 
 
-	newshepard_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, orientation)
+	newshepard_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, 90)
 
 main()
