6c05183068685458b0f979b894a0dd561f9bf107
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-04 21:44:12 -0200

    profile improviment

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -20,7 +20,7 @@ def main():
 	# value			=	-70			# sat-climate I 		-	+/- 02.500 kg
 	# value			=	-70			# engines I				-	+/- 04.529 kg
 
-	# value			=	-78			# deorbit 				-	+/- 04.430 kg
+	value			=	-78			# deorbit 				-	+/- 04.430 kg
 
 	# value			=	-80			# dragao				-	+/- 24.000 kg
 	##		FULL RECOVERY NOT POSSIBLE
@@ -31,7 +31,7 @@ def main():
 	# value			=	-90			# engines II			-	+/- 13.000 kg	
 	# value			=	-90			# abastecimento rcs		-	+/- 06.343 kg
 	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg
-	value			=	-90			# turistas 12 			-	+/- 08.700 kg
+	# value			=	-90			# turistas 12 			-	+/- 08.700 kg
 
 	# value			=	-95			# turistas station 		-	+/- 05.650 kg		
 	# value			=	-95			# ns capsula 			-	+/- 10.105 kg	
