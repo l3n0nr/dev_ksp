ff5fe2c05dfddbb61895497d4e7958767164c42c
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-08 21:59:10 -0200

    profile improviment

--- Python/kerbal/new_gleen/landing.py
+++ Python/kerbal/new_gleen/landing.py
@@ -6,10 +6,11 @@ from base import landing_advanced
 
 def main():
 	#######################################################################################
-	alturaPouso					=			34				# approach the ground
+	# alturaPouso					=			34				# approach the ground
+	alturaPouso					=			40				# approach the ground
 	engines_landing				=			4				# 4/7 engines in landing
-	altitude_landing_burn		=			1200			# altitude for shutdown engines
-	deploy_legs					=			80				# deploy landing legs
+	altitude_landing_burn		=			1000			# altitude for shutdown engines
+	deploy_legs					=			70				# deploy landing legs
 	#######################################################################################
 	#
 	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "New Gleen", False)
