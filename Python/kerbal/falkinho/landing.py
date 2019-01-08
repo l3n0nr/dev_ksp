9deb01733f5e7235e286fad4465a34711e7c2d6d
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-08 11:43:24 -0200

    profile improviment

--- Python/kerbal/falkinho/landing.py
+++ Python/kerbal/falkinho/landing.py
@@ -5,10 +5,12 @@ sys.path.insert(0, '../')
 from base import landing_advanced
 
 def main():
+	###################################################################################################
 	alturaPouso					=			35				# DEFAULT	
 	engines_landing				=			4				# 4/8 engines in landing 		
-	altitude_landing_burn		=			2000			# altitude for shutdown unnecessary engines
+	altitude_landing_burn		=			1500			# altitude for shutdown unnecessary engines
 	deploy_legs					=			100				# deploy landing legs
+	###################################################################################################
 
 	landing_advanced(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Falkinho", True)
 
