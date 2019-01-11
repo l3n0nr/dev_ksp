c62fbe3942ee04df7b2bdc99a7ecf322007f7b93
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-11 17:00:29 -0200

    profile improviment

--- Python/kerbal/velorg/sat_geo.py
+++ Python/kerbal/velorg/sat_geo.py
@@ -6,14 +6,20 @@ sys.path.insert(0, '../')
 from base import velorg
 
 def main():
+	## VELORG I
+	# ########################################################################################
+	# turn_start_altitude     = 16000					# inclination begin
+	# target_altitude         = 100000					# target 
+	# maxq_begin              = 30000					# reduce aceleration stage - begin
+	# ########################################################################################
+
+	## VELORG II
 	########################################################################################
-	turn_start_altitude     = 16000						# inclination begin
-	turn_end_altitude       = 45000						# inclination end
+	turn_start_altitude     = 1000						# inclination begin
 	target_altitude         = 100000					# target 
-	maxq_begin              = 30000						# reduce aceleration stage - begin
-	maxq_end                = 36000						# reduce aceleration stage - end
+	maxq_begin              = 34000						# reduce aceleration stage - begin
 	########################################################################################
 
-	velorg(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, 1, 90)
+	velorg(turn_start_altitude,45000,target_altitude, maxq_begin, 36000, 1, 90)
 
 main()
