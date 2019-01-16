81ab03b79f389fe9ecc96d675914cd0202416c20
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-16 17:31:35 -0200

    profile add

---
+++ Python/kerbal/atlas/asteroid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import atlas
+
+def main():
+	######################################################################################			
+	## DEFAULT 				-			Atlas I 		- 		27.000 kg
+	# maxq_begin              = 34000						# reduce aceleration stage - begin
+	# target_altitude         = 150000					# altitude default			
+	# orientation				= 90						# NORMAL
+	#
+	######################################################################################
+	## ASTEROID 01
+	#
+	maxq_begin              = 30000						# reduce aceleration stage - begin
+	target_altitude         = 150000					# altitude default			
+	orientation				= 225						# NORMAL
+	#
+	######################################################################################
+
+	atlas(2000,45000,target_altitude, maxq_begin, 36000, 1, orientation)
+
+main()
