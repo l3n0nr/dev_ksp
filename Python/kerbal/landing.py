4f31034fe318b98de998081da25fdaa580a6b648
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-19 20:24:02 -0200

    profile improviment

--- Python/kerbal/landing.py
+++ Python/kerbal/landing.py
@@ -2,8 +2,7 @@
 
 ### BASIC
 # import modules
-from base import landing
-landing()
+from base import landing_adv
 
 def main():
 	###################################################################################################
