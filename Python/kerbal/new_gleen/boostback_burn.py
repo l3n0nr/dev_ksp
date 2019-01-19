341c74dc874370b6fb8d42bb025363dca75250b9
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-18 22:00:13 -0200

    profile nuclear engines II improviment

--- Python/kerbal/new_gleen/boostback_burn.py
+++ Python/kerbal/new_gleen/boostback_burn.py
@@ -12,9 +12,11 @@ def main():
 	#
 	# value			=	-155			# Demo. Flight				29.000 kg
 
-	value			=	-155			# Abast.			 		29.000 kg 			???
+	# value			=	-155			# Abast.			 		29.000 kg 
 
 	# value			=	-145			# Nuclear Engines I 		20.000 kg
+
+	value			=	-155			# Nuclear Engines II 		20.000 kg
 
 	# value 		=	-125			# Lander v2					15.000 kg
 	
--- Python/kerbal/new_gleen/landing_zone.py
+++ Python/kerbal/new_gleen/landing_zone.py
@@ -15,17 +15,19 @@ def main():
 	#		X					Value				Profile					Weight	#
 	#################################################################################
 	# PROFILE MISSIONS
-	# taxa					= 	0.14				# Demo Flight			29.000 kg
+	# taxa					= 	0.14				# Demo Flight			29.000 kg				INCONSISTENT
 	### 	29000 / 0.14 = 207142.85
 	### 	29000 * 0.14 = 4060
 
-	taxa					= 	0.23				# Abast. LZ				29.000 kg
+	# taxa					= 	0.23				# Abast. LZ				29.000 kg
 	### 	29000 / 0.23 = 126086.95
 	### 	29000 * 0.23 = 667000
 
-	# taxa					= 	0.17				# Nuclear Engines I 	20.000 kg
+	# taxa					= 	0.17				# Nuclear Engines I 	20.000 kg 				INCONSISTENT
 	###		20000 / 0.17 = 1176.47
 	###		20000 * 0.17 = 3400
+
+	taxa					= 	0.25				# Nuclear Engines I 	20.000 kg
 	
 	# taxa					=	0.28				# Lander v2				15.000 kg
 	###		15000 / 0.28 = 53571.42
@@ -37,6 +39,6 @@ def main():
 
 	#################################################################################
 	#
-	newgleen_landingzone(1000,turn_end_altitude,target_altitude, 28000, 30000, taxa, 90, False)
+	newgleen_landingzone(1000,turn_end_altitude,target_altitude, 28000, 30000, taxa, 90, True)
 
 main()
