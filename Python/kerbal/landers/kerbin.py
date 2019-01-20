c3c2527126186bfd3525dcb3da89bf03d314f9c6
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-20 21:03:46 -0200

    file improviment

--- Python/kerbal/landers/kerbin.py
+++ Python/kerbal/landers/kerbin.py
@@ -6,10 +6,10 @@ from base import landing_adv
 
 def main():
 	###################################################################################################
-	alturaPouso					=			25				# DEFAULT	
+	alturaPouso					=			30				# DEFAULT	
 	engines_landing				=			1				#
 	altitude_landing_burn		=			3000			# altitude for shutdown unnecessary engines
-	deploy_legs					=			200				# deploy landing legs
+	deploy_legs					=			300				# deploy landing legs
 	###################################################################################################
 
 	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Hooper", True)
