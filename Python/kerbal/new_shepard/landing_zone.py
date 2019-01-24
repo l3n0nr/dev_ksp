bc3804b1e5af6e60b4667bc55119f935ce365ea6
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-24 10:28:37 -0200

    file improviment

--- Python/kerbal/new_shepard/landing_zone.py
+++ Python/kerbal/new_shepard/landing_zone.py
@@ -8,11 +8,7 @@ from base import newshepard_landingzone
 def main():	
 	#################################################################################
 	#
-	target_altitude         = 180000					
-	turn_start_altitude     = 2000						
 	turn_end_altitude       = (target_altitude/2)	
-	maxq_begin				= 28000
-	maxq_end				= 30000
 	#
 	#################################################################################
 	#		X					Value				Profile					Weight	#
@@ -20,15 +16,14 @@ def main():
 	#
 	## New Shepard I
 	# taxa					= 0.17					# NS Capsula  		+/- 10.105 kg			
-
+	#
 	## New Shepard II
 	taxa					= 0.19					# NS Capsula 		+/- 10.105 kg
 	## 				Separation first stage: 55km
-
 	#
 	#################################################################################
 
 
-	newshepard_landingzone(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, maxq_end, taxa, 90, True)
+	newshepard_landingzone(2000,turn_end_altitude,180000, 28000, 30000, taxa, 90, True)
 
 main()
