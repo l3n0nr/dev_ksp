7fd9b44f82e665aded1c4c1950587833f822245f
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-25 21:46:35 -0200

    profile lce improviment

--- Python/kerbal/lce/ns_capsula.py
+++ Python/kerbal/lce/ns_capsula.py
@@ -12,6 +12,6 @@ def main():
 	target_altitude         = 150000					# target 	
 	######################################################################################
 
-	lce(2500,45000,target_altitude, maxq_begin, 36000, 1, orientation)
+	lce(2000,45000,target_altitude, maxq_begin, 36000, 1, orientation)
 
 main()
