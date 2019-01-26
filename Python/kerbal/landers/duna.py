fd0875b2815fe5d59e97924935823711c1968539
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-26 20:53:25 -0200

    profile duna add

---
+++ Python/kerbal/landers/duna.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '../')
+from base import landing_adv
+
+def main():
+	###################################################################################################
+	alturaPouso					=			30				# DEFAULT	
+	engines_landing				=			4				#
+	altitude_landing_burn		=			3000			# altitude for shutdown unnecessary engines
+	deploy_legs					=			200				# deploy landing legs
+	###################################################################################################
+
+	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Duna", True)
+
+main()
