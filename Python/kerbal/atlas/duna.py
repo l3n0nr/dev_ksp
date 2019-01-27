0cb1476c5b7704c5473565426fd0cd9a58bfbf39
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-27 09:08:55 -0200

    profile lander improviment

--- Python/kerbal/atlas/duna.py
+++ Python/kerbal/atlas/duna.py
@@ -8,10 +8,14 @@ from base import atlas_x
 def main():
 	######################################################################################
 	## RELAYSAT II
-	maxq_begin              = 25000						# reduce aceleration stage - begin
-	target_altitude         = 150000					# altitude default			
-	orientation				= 90						# NORMAL	
+	# maxq_begin              = 25000						# reduce aceleration stage - begin
+	# target_altitude         = 150000					# altitude default			
 
-	atlas_x(2000,45000,target_altitude, maxq_begin, 36000, 1, orientation)
+	######################################################################################
+	## RELAYSAT II
+	maxq_begin              = 30000						# reduce aceleration stage - begin
+	target_altitude         = 120000					# altitude default				
+
+	atlas_x(2000,45000,target_altitude, maxq_begin, 36000, 1, 90)
 
 main()
