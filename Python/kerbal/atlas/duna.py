3d2519b52329b0ecf9c723fba8c54ec1cc6e9339
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-29 17:13:36 -0200

    profile improviment

--- Python/kerbal/atlas/duna.py
+++ Python/kerbal/atlas/duna.py
@@ -12,9 +12,14 @@ def main():
 	# target_altitude         = 150000					# altitude default			
 
 	######################################################################################
-	## RELAYSAT II
+	## ROVER - DUNA
+	# maxq_begin              = 30000						# reduce aceleration stage - begin
+	# target_altitude         = 120000					# altitude default				
+
+	######################################################################################
+	## ROVER - IKE
 	maxq_begin              = 30000						# reduce aceleration stage - begin
-	target_altitude         = 120000					# altitude default				
+	target_altitude         = 100000					# altitude default				
 
 	atlas_x(2000,45000,target_altitude, maxq_begin, 36000, 1, 90)
 
