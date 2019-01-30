f005731504ab7c6ecd323f4c392fad086a286f03
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-30 11:39:52 -0200

    profile falkinho improviment

--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -43,7 +43,10 @@ def main():
 	# taxa					= 0.23				# abastecimento rcs	-	+/- 06.343 kg
 	# taxa					= 0.23				# deorbit I-II adapter	+/- 03.000 kg
 
-	taxa					= 0.24				# deorbit garra			+/- 03.500 kg
+	# taxa					= 0.24				# deorbit garra			+/- 03.500 kg
+
+	taxa					= 0.24				# sat-climate III		+/- 03.800 kg
+
 	# taxa					= 0.24				# deorbit 			-	+/- 04.345 kg
 	# taxa					= 0.24				# only test landing zone!
 	# taxa					= 0.24				# 2 hub's station	-	+/- 04.550 kg
