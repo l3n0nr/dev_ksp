41b781950fd4249d4d1b30915239cc3c661cdba7
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-16 18:03:48 -0200

    profile new_glenn improviment

--- Python/kerbal/new_glenn/boostback_burn.py
+++ Python/kerbal/new_glenn/boostback_burn.py
@@ -6,21 +6,21 @@ sys.path.insert(0, '../')
 from base import boostback
 
 def main():
-	#########################################################################
-	#	X				Value			Profile						Weight	#
-	#########################################################################
+	################################################################################################################
+	#	X				Value			Profile						Weight				Observations		   	   #
+	################################################################################################################
 	#
-	# value			=	-155			# Demo. Flight				29.000 kg
+	# value			=	-155			# Demo. Flight				29.000 kg 			INCONSISTENT	
+
+	# value			=	-145			# Nuclear Engines I 		20.000 kg 			INCONSISTENT
+
+	# value			=	-170			# Tur + Min 				26.000 kg 			EXCEED MAX WEIGHT
 
 	# value			=	-140			# Abast.			 		25.000 kg
 
-	# value			=	-145			# Nuclear Engines I 		20.000 kg
-
 	# value			=	-155			# Nuclear Engines II 		20.000 kg
 
-	value			=	-170			# Tur + Min 				26.000 kg 				???
-
-	# value			=	-160			# Extrator			 		19.000 kg
+	value			=	-160			# Extrator			 		19.000 kg
 
 	# value 		=	-125			# Lander v2					15.000 kg
 	
