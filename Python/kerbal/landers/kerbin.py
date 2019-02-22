6a512704784094f28babcd62428b2866f43d58cd
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-22 20:12:03 -0300

    profile improviment

--- Python/kerbal/landers/kerbin.py
+++ Python/kerbal/landers/kerbin.py
@@ -7,7 +7,6 @@ from base import landing_adv
 def main():
 	###################################################################################################
 	alturaPouso					=			30				# DEFAULT	
-	# engines_landing				=			1				# Only 1 engine ON
 	engines_landing				=			10				# all engines ON
 	altitude_landing_burn		=			3000			# altitude for shutdown unnecessary engines
 	deploy_legs					=			200				# deploy landing legs
