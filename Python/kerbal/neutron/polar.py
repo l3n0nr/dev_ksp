46ec20beaa23c1dfbd80fcc45a6248e6f372f4bf
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-27 13:48:08 -0300

    profile neutron ii improviment

--- Python/kerbal/neutron/polar.py
+++ Python/kerbal/neutron/polar.py
@@ -6,13 +6,15 @@ sys.path.insert(0, '../')
 from base import neutron
 
 def main():
+	#################################################################
 	## UP
-	# target_altitude			= 130000					#
-	# orientation				= 0 						#
+	# target_altitude			= 130000
+	# orientation				= 0 			
 
 	## DOWN
-	target_altitude			= 130000					#
-	orientation				= 180 						#
+	target_altitude			= 130000
+	orientation				= 180
+	#################################################################
 
 	neutron(1000,45000,target_altitude, 30000, 36000, 1, orientation)
 
