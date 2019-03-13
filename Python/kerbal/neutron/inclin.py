430653d9ae1a74e739e475e26762a1c7d8f47a87
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-13 13:16:01 -0300

    profile improviment

--- Python/kerbal/neutron/inclin.py
+++ Python/kerbal/neutron/inclin.py
@@ -6,23 +6,24 @@ sys.path.insert(0, '../')
 from base import neutron
 
 def main():
-	#################################################################
-	# ## INCLIN TOP
-	# target_altitude			= 110000
-	# orientation				= 41.7
+    #################################################################
+    # target_altitude       =       110000
+    # orientation           =       41.7
 
-	# ## INCLIN DOWN
-	# target_altitude			= 100000
-	# orientation				= 139.2
+    # target_altitude       =       100000
+    # orientation           =       139.2
 
-	# target_altitude 		=		100000
-	# orientation 			=		315
+    # target_altitude       =       100000
+    # orientation           =       315
 
-	target_altitude 		=		105000
-	orientation 			=		160
+    # target_altitude       =       105000
+    # orientation           =       160
 
-	#################################################################
+    target_altitude         =       120000
+    orientation             =       100
 
-	neutron(1000,45000,target_altitude, 30000, 36000, 1, orientation)
+    #################################################################
+
+    neutron(1000,45000,target_altitude, 30000, 36000, 1, orientation)
 
 main()
