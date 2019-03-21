330c66cebc0b0a4bb48c4caed195adacee208e61
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-21 11:09:00 -0300

    profile add

---
+++ Python/kerbal/titan/dres.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import titan_x
+
+def main():
+    ######################################################################################
+    #
+    target_altitude         = 130000                    # altitude default          
+    sepatron 				= 32 						# ???
+    #
+    ######################################################################################
+
+    titan_x(1000,45000,target_altitude, 30000, 36000, 1, 90, sepatron)
+
+main()
