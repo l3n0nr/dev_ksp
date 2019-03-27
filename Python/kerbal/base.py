bc29b16d21b285ca346526a27589147243302411
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-27 10:52:05 -0300

    check profile velorg

--- Python/kerbal/base.py
+++ Python/kerbal/base.py
@@ -2878,14 +2878,16 @@ def velorg(turn_start_altitude,turn_end_altitude,target_altitude, maxq_begin, ma
     print "... Orbital burn manuveur"
     vessel.control.throttle = 1
 
-    time.sleep(burn_time - 0.1)
-    print "... Fine tuning"
-    vessel.control.throttle = 0.25
-    remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
 
-    ## manuveur correction
-    while remaining_burn()[1] > correction_time:
-        pass
+    ## CHECK
+    # time.sleep(burn_time - 0.1)
+    # print "... Fine tuning"
+    # vessel.control.throttle = 0.25
+    # remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
+
+    # ## manuveur correction
+    # while remaining_burn()[1] > correction_time:
+    #     pass
     
     vessel.control.throttle = 0.0
     node.remove()
