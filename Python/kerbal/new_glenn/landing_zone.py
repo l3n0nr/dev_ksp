8fcebeae3746f4e9d0b161afdfe003fdb6ae28f9
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-30 09:32:34 -0300

    profile add

--- Python/kerbal/new_glenn/landing_zone.py
+++ Python/kerbal/new_glenn/landing_zone.py
@@ -21,13 +21,15 @@ def main():
 
 	# taxa					= 	0.205				# Tur + Min				35.000 kg  				EXCEED MAX WEIGHT
 
-	taxa					= 	0.25				# Nuclear II 			20.000 kg
+	# taxa					= 	0.25				# Nuclear II 			20.000 kg
 
 	# taxa					= 	0.25				# Extrator 				19.000 kg 	
 	
 	# taxa					= 	0.25				# Abast. 				25.000 kg	
 
 	# taxa					=	0.28				# Lander v2				15.000 kg
+
+	taxa					=	0.28				# Lander v3				15.000 kg
 	
 	# taxa					= 	0.28				# Adapter + Resourc.	13.500 kg
 
