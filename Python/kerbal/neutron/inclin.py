9a809bfd8b174311105b37b66803081468c86a81
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-04-06 17:12:36 -0300

    profile improviment

--- Python/kerbal/neutron/inclin.py
+++ Python/kerbal/neutron/inclin.py
@@ -7,20 +7,26 @@ from base import neutron
 
 def main():
     #################################################################
-    # target_altitude       =       110000
-    # orientation           =       41.7
+    ## STATION INCLINATION
+        # target_altitude       =       110000
+        # orientation           =       41.7
 
-    # target_altitude       =       100000
-    # orientation           =       139.2
+        # target_altitude       =       100000
+        # orientation           =       139.2
 
+    ## OTHERS
     # target_altitude       =       100000
     # orientation           =       315
 
     # target_altitude       =       105000
     # orientation           =       160
 
-    target_altitude         =       120000
-    orientation             =       100
+    # target_altitude         =       120000
+    # orientation             =       100
+
+    ## RETROGRADE
+    target_altitude         =       100000
+    orientation             =       200
 
     #################################################################
 
