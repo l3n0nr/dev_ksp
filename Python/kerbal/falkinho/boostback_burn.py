b4dbbef4b6fba48136d9383ed30a9d8499173eee
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-04-09 10:30:22 -0300

    profile improviment

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -9,16 +9,14 @@ def main():
 	#########################################################################
 	#	X				Value			Profile						Weight	#
 	#########################################################################
-	#	
-	# value			=	-25			# lander mun v1			-	+/- 19.000 kg
-	##		LANDING ZONE NOT POSSIBLE
-
+	#		
 	# value			= 	-30			# butterfly II			-	+/- 02.920 kg
-	
+		
 	# value			=	-50			# 5 turist + lab		-	+/- 09.500 kg
 	# value			=	-50			# lab + science			-	+/- 05.900 kg
 	# value			=	-50			# minerio 				-	+/- 04.400 kg
 
+	value			=	-60			# engines II nuclear	-	+/- 08.880 kg	
 	# value			=	-60			# extrator conv 		-	+/- 05.700 kg
 	# value			=	-60			# deorbit garra	II		-	+/- 06.000 kg				
 	
@@ -27,23 +25,20 @@ def main():
 	# value			=	-70			# sat-climate I 		-	+/- 02.500 kg
 	# value			=	-70			# engines I				-	+/- 04.529 kg
 	
-	value			=	-80			# deorbit 				-	+/- 04.430 kg		
+	# value			=	-80			# deorbit 				-	+/- 04.430 kg		
 	# value			=	-80			# sat-climate II		- 	+/- 02.500 kg
 	# value			=	-80			# lander mun v2			-	+/- 15.000 kg
 	
 	# value			=	-85			# deorbit garra			-	+/- 03.500 kg
 	# value			=	-85			# sat-climate III		-	+/- 03.800 kg	
-
-	# value			=	-90			# engines II(nucl.)		-	+/- 13.000 kg	
+	
 	# value			=	-90			# abastecimento rcs		-	+/- 06.343 kg
 	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg	
-	# value			=	-90			# turistas 12 			-	+/- 08.700 kg
+	# value			=	-90			# turistas 12 			-	+/- 08.700 kg	
 
 	# value			=	-95			# turistas station 		-	+/- 05.650 kg				
 	# value			=	-95			# turistas deorbit 		-	+/- 08.000 kg				
-	# value			=	-95			# ns capsula 			-	+/- 10.105 kg			
-	# value			=	-95			# dream cheaser			-	+/- 18.352 kg
-	##		LANDING ZONE NOT POSSIBLE
+	# value			=	-95			# ns capsula 			-	+/- 10.105 kg				
 
 	# value			=	-110		# deorbit I-II adapter	-	+/- 03.000 kg
 	# value			=	-110		# abastecimento fuel	-	+/- 11.203 kg
@@ -53,6 +48,17 @@ def main():
 
 	# value			=	80			# falkinho triplo		-	+/- 24.000 kg		
 	# value			=	20			# falkinho triplo teste	-	+/- 36.000 kg
+	#
+	###########################		NOT USED 		#########################
+	#
+	# value			=	-25			# lander mun v1			-	+/- 19.000 kg
+	##		LANDING ZONE NOT POSSIBLE
+	#
+	# value			=	-90			# engines II nuclear	-	+/- 08.880 kg
+	## 		NOT USED THIS
+	#
+	# value			=	-95			# dream cheaser			-	+/- 18.352 kg
+	##		LANDING ZONE NOT POSSIBLE
 	#
 	#########################################################################
 
--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -15,10 +15,8 @@ def main():
 	#################################################################################
 	#		X					Value				Profile					Weight	#
 	#################################################################################
-	#	
-	# taxa					= 0.19				# engines II(nucl.)	-	+/- 13.200 kg	
-	# taxa					= 0.19				# lander mun v1		-	+/- 19.000 kg
-	##		LANDING ZONE NOT POSSIBLE
+	#			
+	taxa					= 0.19				# engines II nuclear-	+/- 08.880 kg
 
 	# taxa					= 0.20				# 5 turist + lab	-	+/- 09.500 kg
 	# taxa					= 0.20				# deorbit garra II		+/- 06.000 kg
@@ -28,31 +26,41 @@ def main():
 	# taxa					= 0.20				# extrator conv I	-	+/- 05.700 kg		
 
 	# taxa					= 0.21				# abastecimento fuel-	+/- 11.203 kg	
-	# taxa					= 0.21				# dragao capsula	-	+/- 11.000 kg
-	# taxa					= 0.21				# dream cheaser		-	+/- 18.352 kg
-	##		LANDING ZONE NOT POSSIBLE
-
+	# taxa					= 0.21				# dragao capsula	-	+/- 11.000 kg	
+		
 	# taxa					= 0.22				# ns capsula 		-	+/- 10.105 kg
 	# taxa					= 0.22				# butterfly II		-	+/- 02.920 kg	
 	# taxa					= 0.22				# turistas station	-	+/- 05.650 kg		
-	# taxa					= 0.22				# turistas deorbit	-	+/- 08.050 kg
-	# taxa					= 0.22				# lander mun v2		-	+/- 15.000 kg
-	##		LANDING ZONE NOT POSSIBLE	
+	# taxa					= 0.22				# turistas deorbit	-	+/- 08.050 kg	
 	# taxa					= 0.22				# ns capsula NTR	-	+/- 10.105 kg
 	## 		RETROGRADE	=	270
-
+	
 	# taxa					= 0.23				# turistas 12 		-	+/- 08.700 kg
 	# taxa					= 0.23				# abastecimento rcs	-	+/- 06.343 kg
 	# taxa					= 0.23				# deorbit I-II adapter	+/- 03.000 kg	
-
+	
 	# taxa					= 0.24				# deorbit garra			+/- 03.500 kg
 	# taxa					= 0.24				# sat-climate III		+/- 03.800 kg
-	taxa					= 0.24				# deorbit 			-	+/- 04.345 kg
+	# taxa					= 0.24				# deorbit 			-	+/- 04.345 kg
 	# taxa					= 0.24				# 2 hub's station	-	+/- 04.550 kg	
-
+		
 	# taxa					= 0.25				# sat-climate		-	+/- 02.500 kg
-
+	
 	# taxa					= 0.26				# science I + HUB	-	+/- 01.820 kg					
+	
+	###########################		NOT USED 		#################################
+	#
+	# taxa					= 0.19				# engines II nuclear-	+/- 08.880 kg
+	## NOT USED THIS
+	#
+	# taxa					= 0.19				# lander mun v1		-	+/- 19.000 kg
+	## LANDING ZONE NOT POSSIBLE
+	#
+	# taxa					= 0.21				# dream cheaser		-	+/- 18.352 kg
+	## LANDING ZONE NOT POSSIBLE
+	#
+	# taxa					= 0.22				# lander mun v2		-	+/- 15.000 kg
+	## LANDING ZONE NOT POSSIBLE	
 	#
 	#################################################################################
 
