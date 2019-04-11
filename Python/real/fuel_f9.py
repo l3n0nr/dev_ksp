883c6c7a2b5a0c559411918204fee21da3a5ae4c
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-04-11 20:18:41 -0300

    profile improviment

--- Python/real/fuel_f9.py
+++ Python/real/fuel_f9.py
@@ -9,7 +9,7 @@
 # LAST MODIFICATION: 21/12/18
 #
 # DESCRIPTION: 
-#   Calculate fuel for reentry of the first stage falcon 9
+#   Calculate fuel for reentry of the first stage Falcon - SpaceX
 #
 # REFERENCE:
 #   <https://www.flightclub.io/build/flightprofile>
@@ -21,28 +21,28 @@
 #################
 #
 # 0(OCISLY) 			- TESS: 9,49%
-#		Weight:  362 kg
+#		Payload mass:  362 kg
 #
 # 0/1(LZ1-LZ2/OCISLY) 	- Falcon Heavy: 40,01% side boosters | 19,61% central core
-#		Weight: 1305 kg
+#		Payload mass: 1305 kg
 #
 # 0(LZ1)				- CRS-12: 16.32%
-#		Weight: 1652 kg
+#		Payload mass: 1652 kg
 #
 # 0(LZ1)				- CRS-13: 16.32%
-#		Weight: 1850 kg
+#		Payload mass: 1850 kg
 #
 # 1(OCISLY)				- Telstar 18V: 6,24%
-#		Weight: 7050 kg
+#		Payload mass: 7050 kg
 #
 # 1(OCISLY)				- Telstar 19V: ????
-#		Weight: 7060 kg
+#		Payload mass: 7060 kg
 #
 # 1(OCISLY)				- Bangabandu: 5,74%
-#		Weight: 3700 kg
+#		Payload mass: 3700 kg
 #
 # 1(JRTI)				- SSO-A: 14.77%
-#		Weight: 4000 kg
+#		Payload mass: 4000 kg
 #
 ###############################################
 # BODY
