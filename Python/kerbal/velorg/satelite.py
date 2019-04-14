f8709cbc5f08d765defe2f0c29bf9a9e5c949eee
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-04-14 15:37:12 -0300

    profile add

---
+++ Python/kerbal/velorg/satelite.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import velorg
+
+def main():
+	########################################################################################
+
+	target_altitude 		= 		130000
+	orientation 			=		315							# TOP-RETROGRADE
+
+	########################################################################################
+
+	velorg(1000,45000,target_altitude, 34000, 36000, 2, orientation)
+
+main()
