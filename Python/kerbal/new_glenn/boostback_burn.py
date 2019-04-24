60fadf72dc8035719c82022602c6f8084dde19f4
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-04-23 23:13:08 -0300

    profile add

--- Python/kerbal/new_glenn/boostback_burn.py
+++ Python/kerbal/new_glenn/boostback_burn.py
@@ -10,21 +10,23 @@ def main():
 	#	X				Value			Profile						Weight				Observations		   	   #
 	################################################################################################################
 	#	
-	value			=	-140			# Abast.			 		25.000 kg
-
-	# value			=	-155			# Nuclear II  				20.000 kg
-
-	# value			=	-160			# Extrator			 		19.000 kg
-
-	# value			=	-150			# CaronaCraft I p1	 		15.500 kg 
-
-	# value			=	-160			# CaronaCraft I p2	 		17.600 kg
-
 	# value 		=	-125			# Lander v2					15.000 kg
 
 	# value 		=	-125			# Lander v3					15.000 kg
 	
 	# value			=	-135			# Adapter + Resourc.		13.500 kg 
+
+	# value			=	-140			# Abast.			 		25.000 kg
+
+	value			=	-145			# Voyager IV - Eevee 		14.400 kg	
+
+	# value			=	-150			# CaronaCraft I p1	 		15.500 kg 
+	
+	# value			=	-155			# Nuclear II  				20.000 kg
+
+	# value			=	-160			# Extrator			 		19.000 kg	
+
+	# value			=	-160			# CaronaCraft I p2	 		17.600 kg	
 	#
 	###########################################		NOT USED 		################################################
 	#
--- Python/kerbal/new_glenn/landing_zone.py
+++ Python/kerbal/new_glenn/landing_zone.py
@@ -16,29 +16,28 @@ def main():
 	#		X					Value				Profile					Weight					Observations  		#
 	#####################################################################################################################
 	#		
-	# taxa					= 	0.25				# Nuclear II 			20.000 kg
+	# taxa					= 	0.25				# Nuclear II 			20.000 kg 				FULL RECOVERY
 
-	# taxa					= 	0.25				# Extrator 				19.000 kg 			
+	# taxa					= 	0.25				# Extrator 				19.000 kg 				FULL RECOVERY
 	
-	taxa					= 	0.25				# Abast. 				25.000 kg	
+	# taxa					= 	0.25				# Abast. 				25.000 kg 				FULL RECOVERY
 
-	# taxa					= 	0.26				# CaronaCraft I p2	 	17.600 kg
+	# taxa					= 	0.26				# CaronaCraft I p2	 	17.600 kg 				(OP) PARCIAL RECOVERY	
 
-	# taxa					= 	0.26				# CaronaCraft I p1	 	15.500 kg
+	# taxa					= 	0.26				# CaronaCraft I p1	 	15.500 kg 				FULL RECOVERY	
 
-	# taxa					=	0.28				# Lander v2				15.000 kg
+	# taxa					=	0.28				# Lander v2				15.000 kg 				FULL RECOVERY
 
-	# taxa					=	0.28				# Lander v3				15.000 kg
+	# taxa					=	0.28				# Lander v3				15.000 kg 				FULL RECOVERY
 
-	# taxa					= 	0.28				# Adapter + Resourc.	13.500 kg		
+	# taxa					= 	0.28				# Adapter + Resourc.	13.500 kg 				FULL RECOVERY
+
+	taxa					= 	0.29				# Voyager IV - Eevee	14.400 kg				(OP) PARCIAL RECOVERY
 	#
-	####################
-	# FULL RECOVERY
-	# 		MIN					0.30 				Landing Zone
-	# 		MAX					0.24 				Landing Zone 			32.000 kg 
-	####################
+	### FULL RECOVERY: MAX(0.30) - MIN(0.24) [Boostback burn + Landing Zone]
+	#####################################################################################################################
 	#
-	###########################		NOT USED 		#################################
+	############################################### 	NOT USED 		#################################################
 	#
 	# taxa					= 	0.14				# Demo Flight			29.000 kg				INCONSISTENT
 
@@ -48,7 +47,7 @@ def main():
 
 	# taxa					= 	0.29				# DeOrbitGarra II		05.200 kg 				NOT IDEAL - GTO	
 	#
-	#################################################################################
+	#####################################################################################################################
 	#
 	newglenn_landingzone(1000,turn_end_altitude,target_altitude, 28000, 30000, taxa, orientation, True)
 
