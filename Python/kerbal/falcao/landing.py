85730b9f7b2c7fadf1bcf3e1034939f0b7a70540
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-10 16:20:14 -0300

    profile improvements

--- Python/kerbal/falcao/landing.py
+++ Python/kerbal/falcao/landing.py
@@ -8,8 +8,8 @@ def main():
 	###################################################################################################
 	alturaPouso					=			40				# landing way 		- 		GENERIC
 	engines_landing				=			12				# engines on 
-	altitude_landing_burn		=			2000			# altitude for shutdown unnecessary engines
-	deploy_legs					=			200				# deploy landing legs
+	altitude_landing_burn		=			2500			# altitude for shutdown unnecessary engines
+	deploy_legs					=			300				# deploy landing legs
 	###################################################################################################
 
 	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Falkinho", True)
