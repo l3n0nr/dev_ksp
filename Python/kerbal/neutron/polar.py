818b81d6e7e209b755e061c81063666bc421c633
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-11-18 22:57:14 -0200

    file improviment

--- Python/kerbal/neutron/polar.py
+++ Python/kerbal/neutron/polar.py
@@ -8,15 +8,13 @@ from base import neutron
 def main():
 	#################################################################
 	## UP
-	# target_altitude			= 130000
-	# orientation				= 0 			
+	orientation				= 0 			
 
 	# DOWN
-	target_altitude			= 130000
-	orientation				= 180
+	# orientation				= 180
 
 	#################################################################
 
-	neutron(1000,45000,target_altitude, 30000, 36000, 1, orientation)
+	neutron(1000,45000,130000, 30000, 36000, 1, orientation)
 
 main()
