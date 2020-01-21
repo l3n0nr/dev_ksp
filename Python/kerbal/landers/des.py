f39ade1ed2f58613672a6995b536eda19840f56c
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-01-21 16:48:41 -0200

    profile add

---
+++ Python/kerbal/landers/des.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '../')
+from base import landing_adv
+
+def main():
+	###################################################################################################
+	alturaPouso					=			20				# DEFAULT	
+	deploy_legs					=			100				# deploy landing legs
+	###################################################################################################
+
+	landing_adv(alturaPouso, 1, 0, deploy_legs, "Lander", True)
+
+main()
