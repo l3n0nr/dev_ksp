2c3a316ce9cba4c0c4ace896daa6d3272c76b9cd
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-03-07 23:42:28 -0300

    profile lab + science changed

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -12,30 +12,30 @@ def main():
 	#		
 	# value			= 	-30			# butterfly II			-	+/- 02.920 kg
 		
-	value			=	-45			# 6 kerbals + ISRU		-	+/- 09.600 kg	
+	# value			=	-45			# 6 kerbals + ISRU		-	+/- 09.600 kg	
 	
-	# value			=	-50			# 5 turist + lab		-	+/- 09.500 kg
-	# value			=	-50			# lab + science			-	+/- 05.900 kg
 	# value			=	-50			# minerio 				-	+/- 04.400 kg
+	value			=	-50			# lab + science			-	+/- 06.360 kg
+	# value			=	-50			# 5 turist + lab		-	+/- 09.500 kg
 
-	# value			=	-60			# engines II nuclear	-	+/- 08.880 kg	
 	# value			=	-60			# extrator conv I 		-	+/- 05.700 kg
 	# value			=	-60			# deorbit garra	II		-	+/- 06.000 kg				
+	# value			=	-60			# engines II nuclear	-	+/- 08.880 kg	
 	
 	# value			=	-65			# science I + HUB		-	+/- 01.820 kg	
 
 	# value			=	-70			# sat-climate I 		-	+/- 02.500 kg
 	# value			=	-70			# engines I				-	+/- 04.529 kg
 	
-	# value			=	-80			# deorbit 				-	+/- 04.430 kg		
 	# value			=	-80			# sat-climate II		- 	+/- 02.500 kg	
+	# value			=	-80			# deorbit 				-	+/- 04.430 kg		
 	
 	# value			=	-85			# deorbit garra I/II	-	+/- 03.500 kg
 	
 	# value			=	-90			# sat-climate III		-	+/- 03.900 kg		
+	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg	
 	# value			=	-90			# deorbit garra III		-	+/- 06.300 kg
 	# value			=	-90			# abastecimento rcs		-	+/- 06.343 kg
-	# value			=	-90			# 2 hub's station 		-	+/- 04.550 kg	
 	# value			=	-90			# turistas 12 			-	+/- 08.700 kg	
 
 	# value			=	-95			# turistas station 		-	+/- 05.650 kg				
@@ -43,8 +43,8 @@ def main():
 	# value			=	-95			# ns capsula 			-	+/- 10.105 kg				
 
 	# value			=	-110		# deorbit I-II adapter	-	+/- 03.000 kg
-	# value			=	-110		# abastecimento fuel	-	+/- 11.203 kg
 	# value			=	-110		# dragao capsula 		-	+/- 11.000 kg
+	# value			=	-110		# abastecimento fuel	-	+/- 11.203 kg
 	#
 	###########################		NOT USED 		#########################
 	#
--- Python/kerbal/falkinho/landing_zone.py
+++ Python/kerbal/falkinho/landing_zone.py
@@ -18,26 +18,27 @@ def main():
 	#			
 	# taxa					= 0.19				# engines II nuclear-	+/- 08.880 kg
 
-	taxa					= 0.20				# 6kerbals + ISRU	-	+/- 09.600 kg
-	# taxa					= 0.20				# 5 turist + lab	-	+/- 09.500 kg
-	# taxa					= 0.20				# deorbit garra II		+/- 06.000 kg
-	# taxa					= 0.20				# engines I			-	+/- 04.529 kg
 	# taxa					= 0.20				# minerio			-	+/- 04.400 kg
-	# taxa					= 0.20				# lab + science		-	+/- 05.900 kg	
+	# taxa					= 0.20				# engines I			-	+/- 04.529 kg
 	# taxa					= 0.20				# extrator conv I	-	+/- 05.700 kg		
+	# taxa					= 0.20				# deorbit garra II		+/- 06.000 kg
+	taxa					= 0.20				# lab + science		-	+/- 06.360 kg	
 
-	# taxa					= 0.21				# abastecimento fuel-	+/- 11.203 kg	
+	# taxa					= 0.20				# 5 turist + lab	-	+/- 09.500 kg
+	# taxa					= 0.20				# 6kerbals + ISRU	-	+/- 09.600 kg
+	
 	# taxa					= 0.21				# dragao capsula	-	+/- 11.000 kg	
+	# taxa					= 0.21				# abastecimento fuel-	+/- 11.203 kg	
 		
-	# taxa					= 0.22				# ns capsula 		-	+/- 10.105 kg
 	# taxa					= 0.22				# butterfly II		-	+/- 02.920 kg	
 	# taxa					= 0.22				# turistas station	-	+/- 05.650 kg		
 	# taxa					= 0.22				# turistas deorbit	-	+/- 08.050 kg		
+	# taxa					= 0.22				# ns capsula 		-	+/- 10.105 kg
 	
-	# taxa					= 0.23				# deorbit garra	III		+/- 06.300 kg
-	# taxa					= 0.23				# turistas 12 		-	+/- 08.700 kg	
-	# taxa					= 0.23				# abastecimento rcs	-	+/- 06.343 kg
 	# taxa					= 0.23				# deorbit I-II adapter	+/- 03.000 kg		
+	# taxa					= 0.23				# deorbit garra	III		+/- 06.300 kg
+	# taxa					= 0.23				# abastecimento rcs	-	+/- 06.343 kg
+	# taxa					= 0.23				# turistas 12 		-	+/- 08.700 kg	
 	
 	# taxa					= 0.24				# deorbit garra I/II	+/- 03.500 kg	
 	# taxa					= 0.24				# sat-climate III		+/- 03.900 kg
