475a5927b3b28606bb5d2c35dd9d94ae0a9bdf03
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-03-22 17:39:22 -0300

    profile changed - resume

--- Python/kerbal/atlas/jool.py
+++ Python/kerbal/atlas/jool.py
@@ -8,9 +8,7 @@ from base import atlas_x
 def main():
 	######################################################################################
 	#
-	maxq_begin              = 30000						# reduce aceleration stage - begin
-	target_altitude         = 100000					# altitude default				
+	atlas_x(2000,45000,110000, 34000, 36000, 1, 90, 0)
 	#
-	atlas_x(2000,45000,target_altitude, maxq_begin, 36000, 1, 90, 0)
-
+	######################################################################################
 main()
