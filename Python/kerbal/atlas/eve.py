1e38d233d97023b37c2607fc1ee474518e0b2cd8
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-03-27 10:57:39 -0300

    profile add

---
+++ Python/kerbal/atlas/eve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import atlas_x
+
+def main():
+	######################################################################################
+	## ROVER - Eevee
+	# maxq_begin              = 30000					# reduce aceleration stage - begin
+	# target_altitude         = 100000				# altitude default				
+
+	######################################################################################
+	## ROVER - Gilly
+	maxq_begin              = 32000					# reduce aceleration stage - begin
+	target_altitude         = 100000				# altitude default		
+
+	######################################################################################
+
+	atlas_x(2000,45000,target_altitude, maxq_begin, 36000, 1, 90, 0)
+
+main()
