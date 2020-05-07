69ba757cd36f057479a67bda54bfb955d7e87e15
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-05-07 17:51:13 -0300

    profile improviment

--- Python/kerbal/velorg/satelite.py
+++ Python/kerbal/velorg/satelite.py
@@ -9,25 +9,25 @@ def main():
 	########################################################################################
 
 	## PROGRADE
-	### normal
+		### normal
 		# target_altitude       =       130000
 		# orientation           =       90
 
-	### down + 45(right) 
+		### down + 45(right) 
 		# target_altitude       =       130000
 		# orientation           =       135
 
 	## RETROGRADE
-	### top
-		# target_altitude       =       130000
-		# orientation           =       315
+		### top
+		target_altitude       	=       130000
+		orientation           	=       315
 
-	### down + 45(left)
-	target_altitude         =       100000
-	orientation             =       225
+		### down + 45(left)
+		# target_altitude         =       100000
+		# orientation             =       225
 
 	########################################################################################
 
-	velorg(1000,45000,target_altitude,34000,36000,3,orientation)
+	velorg(1000,45000,target_altitude,34000,36000,5,orientation)
 
 main()
