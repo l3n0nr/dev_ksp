77069d4491efd2c0e0d9a4a35d0f9a0d17fd146e
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-05-18 10:35:57 -0300

    file improviment

--- Python/kerbal/velorg/satelite.py
+++ Python/kerbal/velorg/satelite.py
@@ -6,28 +6,28 @@ sys.path.insert(0, '../')
 from base import velorg
 
 def main():
-	########################################################################################
+    ########################################################################################
 
-	## PROGRADE
-		### normal
-		# target_altitude       =       130000
-		# orientation           =       90
+    ## PROGRADE
+    ### normal
+    # target_altitude       =       130000
+    # orientation           =       90
 
-		### down + 45(right) 
-		# target_altitude       =       130000
-		# orientation           =       135
+    ### down + 45(right) 
+    target_altitude         =       130000
+    orientation             =       135    
 
-	## RETROGRADE
-		### top
-		target_altitude       	=       130000
-		orientation           	=       315
+    ## RETROGRADE
+    ### top
+    # target_altitude           =       130000
+    # orientation               =       315
 
-		### down + 45(left)
-		# target_altitude         =       100000
-		# orientation             =       225
+    ### down + 45(left)
+    # target_altitude         =       100000
+    # orientation             =       225
 
-	########################################################################################
+    ########################################################################################
 
-	velorg(1000,45000,target_altitude,34000,36000,5,orientation)
+    velorg(1000,45000,target_altitude,34000,36000,5,orientation)
 
 main()
