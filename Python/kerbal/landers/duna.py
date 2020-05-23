8dd3c7da9ec262a7238193f9d8ebabf6ca77fb96
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-05-22 21:23:50 -0300

    profile improviment

--- Python/kerbal/landers/duna.py
+++ Python/kerbal/landers/duna.py
@@ -6,10 +6,10 @@ from base import landing_adv
 
 def main():
 	###################################################################################################
-	alturaPouso					=			30				# DEFAULT	
+	alturaPouso					=			35				# DEFAULT	
 	engines_landing				=			4				#
 	altitude_landing_burn		=			0				# altitude for shutdown unnecessary engines
-	deploy_legs					=			0				# deploy landing legs
+	deploy_legs					=			250				# deploy landing legs
 	###################################################################################################
 
 	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Lander", True)
