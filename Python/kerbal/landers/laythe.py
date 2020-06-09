bdf82a6114798011f35f55731424166ee4f8a628
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-06-09 20:42:52 -0300

    profile add

---
+++ Python/kerbal/landers/laythe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '../')
+from base import landing_adv
+
+def main():
+	###################################################################################################
+	alturaPouso					=			30				# DEFAULT	
+	engines_landing				=			50				# ALL engines - MAX	
+	altitude_landing_burn		=			3000			# altitude for shutdown unnecessary engines
+	deploy_legs					=			200				# deploy landing legs
+	###################################################################################################
+
+	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Lander", True)
+
+main()
