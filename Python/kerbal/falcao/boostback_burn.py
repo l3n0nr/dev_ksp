d25703c1568e670d60f0ae4b29291a53a5ef2d0e
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-06-17 19:43:59 -0300

    profile add

---
+++ Python/kerbal/falcao/boostback_burn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import boostback
+
+def main():
+	##################################################################################
+	#	X				Value			Profile						Weight orbiter	 #
+	##################################################################################
+	#	
+	value			=	-180			# Flight Test Cargo				?? kg 		 #
+	#
+	##################################################################################
+
+	boostback(value)
+
+main()
---
+++ Python/kerbal/falcao/landing_zone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# import module
+import sys
+sys.path.insert(0, '../')
+from base import falcao_landing_zone
+
+def main():	
+	#################################################################################
+	#
+	target_altitude         = 150000
+	turn_end_altitude       = (target_altitude/1.5)		
+	#
+	##########################################################################################
+	#		X					Value				Profile					Weight orbiter	 #
+	##########################################################################################
+	#	
+	taxa					= 	0.20				# Flight Test Cargo			?? kg		 #
+	#
+	##########################################################################################
+
+	falcao_landing_zone(2000,turn_end_altitude,target_altitude, 28000, 30000, taxa, 90, False)
+
+main()
