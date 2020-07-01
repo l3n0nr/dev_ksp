0c205082659ac5976c4f033db8de802972ac7dd1
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-06-30 22:25:21 -0300

    profile changed

--- Python/kerbal/falkinho/landing_zone_triplo.py
+++ Python/kerbal/falkinho/landing_zone_triplo.py
@@ -9,11 +9,11 @@ def main():
 
     ###############################################################################
     #
-    
-    # target_altitude         = 280000               
-    # taxa                  = 0.20              # DEMO            +/- 21.000 kg
+    # DEMO              21.000 kg
+    # target_altitude       = 280000               
+    # taxa                  = 0.20              
 
-    # DeOrbit IIII = 07.125 kg
+    # DeOrbit IIII      07.125 kg
     target_altitude       = 200000                
     taxa                  = 0.24              
 
@@ -22,6 +22,6 @@ def main():
     #
     ###############################################################################
     #
-    falkinho_landing_zone(1000,215000,280000, 26000, 36000, taxa, 90, True)
+    falkinho_landing_zone(1000,213333,280000, 26000, 36000, taxa, 90, True)
 
 main()
