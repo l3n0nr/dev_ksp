ce84fe6b403fbe9a765090e01fd3dcb91d038981
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-07-17 10:20:23 -0300

    profile improviment

--- Python/kerbal/lce/exp_cap.py
+++ Python/kerbal/lce/exp_cap.py
@@ -7,10 +7,15 @@ from base import lce
 
 def main():	
 	######################################################################################
-	## NORMAL
-	orientation				= 90						
-	maxq_begin 				= 25000						# throtle down
-	target_altitude         = 160000					# target 	
+	# ## NORMAL
+	# orientation				= 90						
+	# maxq_begin 				= 25000						# throtle down
+	# target_altitude         = 160000					# target 	
+
+	## RETROGRADE
+	orientation				= 270						
+	maxq_begin 				= 28000						# throtle down
+	target_altitude         = 120000					# target 	
 	######################################################################################
 
 	lce(2000,45000,target_altitude, maxq_begin, 36000, 1, orientation)
