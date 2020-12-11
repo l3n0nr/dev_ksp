c921d70626f7206140fa05123fa8daded32cd583
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-12-11 14:11:39 -0300

    profile landing new shepard improviment

--- Python/kerbal/base.py
+++ Python/kerbal/base.py
@@ -3306,9 +3306,11 @@ def landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs
 
                     vessel.control.sas_mode = vessel.control.sas_mode.anti_target
                 # nao encontrou o alvo - the beast landing
-                elif not target_vessel and profile=="Falcao" or profile=="Falkinho Triplo" or profile=="New Glenn" or surAlt <= 200:                                        
+                elif not target_vessel and profile=="Falcao" or profile=="Falkinho Triplo" or profile=="New Glenn" or profile=="New Shepard":                                        
                     if surAlt <= altitude_landing_burn:
                         vessel.control.sas_mode = vessel.control.sas_mode.radial
+                    else:
+                        vessel.control.sas_mode = vessel.control.sas_mode.retrograde
                 # qualquer outra coisa
                 else:
                     vessel.control.sas_mode = vessel.control.sas_mode.retrograde
