0bac0a8bbae877abb0cecc712f9cef45a25b3589
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-12-11 11:12:56 -0300

    profile add

--- Python/kerbal/falkinho/boostback_burn.py
+++ Python/kerbal/falkinho/boostback_burn.py
@@ -11,11 +11,11 @@ def main():
 	#########################################################################
 	#			
 	# value			=	-45			# 6 kerbals + ISRU		-	+/- 09.600 kg	
-	# value			=	-50			# minerio 				-	+/- 04.400 kg		
-	# value			=	-90			# sat climate			-	+/- ? kg				
+	# value			=	-50			# minerio 				-	+/- 04.400 kg	
 	# value			=	-60			# engines II nuclear	-	+/- 08.880 kg			
 	# value			=	-90			# deorbit garra IIII	-	+/- 07.125 kg					
-	value			=	-115		# dragao capsula II		-	+/- 12.300 kg
+	# value			=	-115		# dragao capsula II		-	+/- 12.300 kg
+	value			=	-115		# sat climate			-	+/- 03.050 kg	
 	#
 	###########################		LEGACY 			#########################
 	#
