9eaf0c4f953f2c8813530b8e263276978c456694
Lenon Ricardo <lenonrmsouza@gmail.com>
2021-02-12 15:44:11 -0300

    perfil melhorado

--- Python/kerbal/landers/eve.py
+++ Python/kerbal/landers/eve.py
@@ -6,10 +6,11 @@ from base import landing_adv
 
 def main():
 	###################################################################################################
-	alturaPouso					=			40				# DEFAULT	
+	# alturaPouso					=			40				# DEFAULT	
+	alturaPouso					=			30				# FALCAO	
 	engines_landing				=			50				# ALL engines - MAX	
 	altitude_landing_burn		=			3000			# altitude for shutdown unnecessary engines
-	deploy_legs					=			200				# deploy landing legs
+	deploy_legs					=			150				# deploy landing legs
 	###################################################################################################
 
 	landing_adv(alturaPouso, engines_landing, altitude_landing_burn, deploy_legs, "Lander", True)
