efe24036a39134a51c3756dbd4ca9530bd0a5256
Lenon Ricardo <lenonrmsouza@gmail.com>
2021-02-26 14:45:23 -0300

    files otimized

--- Python/kerbal/new_glenn/landing_zone.py
+++ Python/kerbal/new_glenn/landing_zone.py
@@ -16,31 +16,12 @@ def main():
 	#		X					Value				Profile					Weight					RECOVERY  			#
 	#####################################################################################################################
 	#		
-	# taxa					= 	0.20				# Dream Cheaser I 		15.000 kg 				FULL
-
-	# taxa					= 	0.25				# Adapter + Resourc.	13.500 kg 				FULL 			**
-	# taxa					= 	0.25				# Extrator 				19.000 kg 				FULL
-	# taxa					= 	0.25				# Nuclear II 			20.000 kg 				FULL
 	taxa					= 	0.25				# Abastecimento			25.000 kg 				FULL
-
-	# taxa					= 	0.26				# CaronaCraft I p1	 	15.500 kg 				FULL
-	# taxa					= 	0.26				# CaronaCraft I p2	 	17.600 kg 				PARCIAL
-
-	# taxa					=	0.28				# Lander v2	/ v3		15.000 kg 				FULL
-
 	# taxa					= 	0.29				# Voyager IV			14.400 kg				PARCIAL
 	# taxa					= 	0.29				# Deep Relay I 			14.500 kg				FULL
 	#
-	#
 	### PARCIAL RECOVERY: 	ONLY FIRST STAGE
 	### FULL RECOVERY: 		MAX(0.32) - MIN(0.20) [Boostback burn + Landing Zone]
-	#
-	############################################### 	NOT USED 		#################################################
-	#
-	# taxa					= 	0.14				# Demo Flight			29.000 kg				INCONSISTENT
-	# taxa					= 	0.17				# Nuclear I 			20.000 kg 				INCONSISTENT
-	# taxa					= 	0.205				# Tur + Min				35.000 kg  				EXCEED MAX WEIGHT	
-	# taxa					= 	0.29				# DeOrbitGarra II		05.200 kg 				NOT IDEAL - GTO	
 	#
 	#####################################################################################################################
 	#
