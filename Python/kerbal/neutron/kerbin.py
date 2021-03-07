04c2e9429c391b481039a285fc1b96f65771c82c
Lenon Ricardo <lenonrmsouza@gmail.com>
2021-03-07 10:20:49 -0300

    profile improviment

--- Python/kerbal/neutron/kerbin.py
+++ Python/kerbal/neutron/kerbin.py
@@ -6,6 +6,7 @@ sys.path.insert(0, '../')
 from base import neutron
 
 def main():
+	#
 	#################################################################
 	# PROGRADE
 	target_altitude			= 150000
@@ -14,8 +15,18 @@ def main():
 	# RETROGRADE
 	# target_altitude			= 90000
 	# orientation				= 270
-	#################################################################
 
+	## CUBESATS
+	# target_altitude         	= 125000
+	# orientation				= 10	
+	#################################################################
+	#
+	#	INCLINATION ORBITS:
+	#		UP: 	Inclination - Relative inclination to target
+	#		DOWN: 	Inclination + Relative inclination to target
+	#
+	#################################################################
+	#
 	neutron(1000,45000,target_altitude, 30000, 36000, 1, orientation, True)
 
 main()
